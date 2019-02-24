ace80781de7d2efe3e0405dbcc64eed252375e75
Fred Wehle <fredmon3@gmail.com>
2019-02-24 16:32:32 -0700

    Add prettier with commit hook and vscode helpers (#16)
    * Add prettier with commit hook and vscode helpers
    
    * add prettier format on save

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
 
 class App extends Component {
   render() {
