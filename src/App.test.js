229deac22ba877c90bbd026bbc109dda66f2c67f
Jacob <jacob.venable@gmail.com>
2019-03-23 19:40:15 -0600

    Update App test to test for finding elements

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import { Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Shelters from './components/Shelters';
+import Hygiene from './components/Hygiene';
+import Hotlines from './components/Hotlines';
+import Food from './components/Food';
+import Transit from './components/Transit';
 
-it('renders without crashing', () => {
-  shallow(<App />);
+describe('<App />', () => {
+  const wrapper = shallow(<App />);
+  const routes = [Home, Shelters, Hygiene, Hotlines, Food, Transit];
+  it('renders properly', () => {
+    expect(wrapper.find('div').length).toBe(1);
+    expect(wrapper.find(Header).length).toBe(1);
+    expect(wrapper.find(Route).length).toBe(routes.length);
+  });
 });
