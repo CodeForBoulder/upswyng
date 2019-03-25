cf769058c92e5f723fdd96b26c104e5e728c0304
Jacob Venable <jacobvenable@Jacobs-MacBook-Pro.local>
2019-03-25 14:20:43 -0600

    Update Search component test.
    Split separate testing rendered components into separate blocks.

--- src/components/Search.test.js
+++ src/components/Search.test.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Search from './Search';
-import { TextField, InputAdornment } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
+jest.mock('@material-ui/core/TextField', () => 'TextField');
 
 describe('<Search />', () => {
   const wrapper = mount(<Search />);
-  it('renders properly', () => {
+  it('renders a required form element', () => {
     expect(wrapper.find('form').length).toBe(1);
-    expect(wrapper.find(TextField).length).toBe(1);
-    expect(wrapper.find(InputAdornment).length).toBe(1);
-    expect(wrapper.find(SearchIcon).length).toBe(1);
+  });
+  it('renders a TextField', () => {
+    expect(wrapper.find('TextField').length).toBe(1);
   });
 });
