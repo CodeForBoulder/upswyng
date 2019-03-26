97659ae12ac203e31d049d37f9eae75343c73157
Jacob Venable <jacob.venable@gmail.com>
2019-03-26 13:19:04 -0600

    Add buttons to the home page.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,115 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, IconButton } from '@material-ui/core';
+import CallIcon from '@material-ui/icons/Call';
+import RestaurantIcon from '@material-ui/icons/Restaurant';
+import HomeIcon from '@material-ui/icons/Home';
+import HotTubIcon from '@material-ui/icons/HotTub';
+import BusIcon from '@material-ui/icons/DirectionsBus';
+import GroceryStoreIcon from '@material-ui/icons/LocalGroceryStore';
+import HealingIcon from '@material-ui/icons/Healing';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import PetsIcon from '@material-ui/icons/Pets';
+import WifiIcon from '@material-ui/icons/Wifi';
+import WorkIcon from '@material-ui/icons/Work';
+import InfoIcon from '@material-ui/icons/Info';
 import Search from './Search';
 
-const Home = () => (
+export const HomeButtonsMajor = [
+  {
+    text: 'Hotlines',
+    icon: CallIcon
+  },
+  {
+    text: 'Food',
+    icon: RestaurantIcon
+  },
+  {
+    text: 'Shelter',
+    icon: HomeIcon
+  },
+  {
+    text: 'Hygiene',
+    icon: HotTubIcon
+  },
+  {
+    text: 'Transit',
+    icon: BusIcon
+  },
+  {
+    text: 'Resources',
+    icon: GroceryStoreIcon
+  }
+];
+
+export const HomeButtonsMinor = [
+  {
+    text: 'Health',
+    icon: HealingIcon
+  },
+  {
+    text: 'Education',
+    icon: LibraryBooksIcon
+  },
+  {
+    text: 'Pets',
+    icon: PetsIcon
+  },
+  {
+    text: 'Wifi & Tech',
+    icon: WifiIcon
+  },
+  {
+    text: 'Work',
+    icon: WorkIcon
+  },
+  {
+    text: 'Social Services',
+    icon: InfoIcon
+  }
+];
+
+export const HomeComponent = () => (
   <Grid container className="home" spacing={16} justify="space-evenly">
-    <Grid component={Search} item xs={12} />
+    <Grid item xs={12}>
+      <Grid container className="home" spacing={16} justify="space-evenly">
+        <Grid component={Search} item xs={12} />
+      </Grid>
+    </Grid>
+    <Grid item xs={12}>
+      <Grid container spacing={16} justify="space-evenly">
+        {HomeButtonsMajor.map((button, index) => {
+          return (
+            <Grid item xs={4} key={button.text}>
+              <Grid container direction="column" align="center">
+                <IconButton color="primary">
+                  <button.icon />
+                  {button.text}
+                </IconButton>
+              </Grid>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Grid>
+    <Grid item xs={12}>
+      <Grid container spacing={16} justify="space-evenly">
+        {HomeButtonsMinor.map(button => {
+          return (
+            <Grid item xs={4} key={button.text}>
+              <Grid container direction="column" align="center">
+                <IconButton color="secondary">
+                  <button.icon />
+                  {button.text}
+                </IconButton>
+              </Grid>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Grid>
   </Grid>
 );
+
+const Home = () => <HomeComponent />;
 
 export default Home;
--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -1,17 +1,69 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Home from './Home';
+import { HomeComponent, HomeButtonsMajor, HomeButtonsMinor } from './Home';
 jest.mock('@material-ui/core/Grid', () => 'Grid');
+jest.mock('@material-ui/core/IconButton', () => 'IconButton');
+jest.mock('@material-ui/icons/Call', () => 'CallIcon');
+jest.mock('@material-ui/icons/Restaurant', () => 'RestaurantIcon');
+jest.mock('@material-ui/icons/Home', () => 'HomeIcon');
+jest.mock('@material-ui/icons/HotTub', () => 'HotTubIcon');
+jest.mock('@material-ui/icons/DirectionsBus', () => 'BusIcon');
+jest.mock('@material-ui/icons/LocalGroceryStore', () => 'GroceryStoreIcon');
+jest.mock('@material-ui/icons/Healing', () => 'HealingIcon');
+jest.mock('@material-ui/icons/LibraryBooks', () => 'LibraryBooksIcon');
+jest.mock('@material-ui/icons/Pets', () => 'PetsIcon');
+jest.mock('@material-ui/icons/Wifi', () => 'WifiIcon');
+jest.mock('@material-ui/icons/Work', () => 'WorkIcon');
+jest.mock('@material-ui/icons/Info', () => 'InfoIcon');
 jest.mock('./Search', () => 'Search');
 
 describe('<Home/>', () => {
-  const wrapper = shallow(<Home />);
+  const wrapper = shallow(<HomeComponent />);
+
+  const numSearchGridComponents = 3;
+  const numButtonsMajorGridComponents = 2 + HomeButtonsMajor.length * 2;
+  const numButtonsMinorGridComponents = 2 + HomeButtonsMinor.length * 2;
   it('renders the proper number of layout components', () => {
-    expect(wrapper.find('Grid').length).toBe(2);
+    expect(wrapper.find('Grid').length).toBe(
+      1 +
+        numSearchGridComponents +
+        numButtonsMajorGridComponents +
+        numButtonsMinorGridComponents
+    );
   });
   it('renders one layout component as the search component', () => {
     expect(
       wrapper.findWhere(n => n.prop('component') === 'Search').length
     ).toBe(1);
+  });
+  it('renders all major buttons and only once', () => {
+    HomeButtonsMajor.forEach(button => {
+      let matchingButtons = wrapper
+        .findWhere(child => {
+          return (
+            child.type() === 'IconButton' &&
+            child.text() === button.text &&
+            child.find(`${button.icon}`).length === 1
+          );
+        })
+        .map(iconButton => iconButton.text());
+      expect(matchingButtons).toContain(button.text);
+      expect(matchingButtons.length).toBe(1);
+    });
+  });
+  it('renders all minor buttons and only once', () => {
+    HomeButtonsMinor.forEach(button => {
+      let matchingButtons = wrapper
+        .findWhere(child => {
+          return (
+            child.type() === 'IconButton' &&
+            child.text() === button.text &&
+            child.find(`${button.icon}`).length === 1
+          );
+        })
+        .map(iconButton => iconButton.text());
+      expect(matchingButtons).toContain(button.text);
+      expect(matchingButtons.length).toBe(1);
+    });
   });
 });
