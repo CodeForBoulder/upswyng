6d7acc49b3f5d5366dcd32978501767ac3888f49
Jacob Venable <jacob.venable@gmail.com>
2019-03-29 11:49:10 -0600

    Add dummy data object to Resource

--- src/components/Resource.test.js
+++ src/components/Resource.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Resource from './Resource';
+import { resourceApiReturn, ResourceComponent } from './Resource';
 
 describe('<Resource/>', () => {
-  const wrapper = shallow(<Resource />);
+  const wrapper = shallow(<ResourceComponent />);
   it('renders the container', () => {
     expect(wrapper.find('div').length).toBe(1);
   });
