84ade5cc218f3ee722b00d0f6f916a09f65905eb
Jacob Venable <jacob.venable@gmail.com>
2019-03-29 15:53:13 -0600

    Remove ID from resourceApiReturn in Carousel.
    Add the resource ID as a dummy variable exported from Carousel.
    
    Add images array to dummy data in Carousel.

--- src/components/Resource.test.js
+++ src/components/Resource.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Resource, { singleResourceApiReturn } from './Resource';
+import Resource, { singleResourceId } from './Resource';
 jest.mock('./Carousel', () => 'Carousel');
 
 describe('<Resource/>', () => {
-  const props = { singeResourceApiReturn: singleResourceApiReturn };
+  const props = { id: singleResourceId };
   const wrapper = shallow(<Resource {...props} />);
-  console.log(wrapper.debug());
   it('renders all required information', () => {
     expect(wrapper.find('Carousel').length).toBe(1);
   });
