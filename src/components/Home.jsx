073e9b1adee32e3967ae6475ddfc267c4e51f562
Jacob Venable <jacobvenable@Jacobs-MacBook-Air.local>
2019-04-07 12:12:25 -0600

    Move button rendering to helper function.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,25 @@ import HomeButtonsMajor from './HomeButtonsMajor';
 import HomeButtonsMinor from './HomeButtonsMinor';
 import Search from './Search';
 
+const renderButtons = (buttons, props) =>
+  buttons.map((button, index) => {
+    return (
+      <Grid item xs={4} key={button.text}>
+        <Grid container direction="column" align="center">
+          <Button
+            component={Link}
+            to={button.to}
+            data-test={button.text}
+            {...props}
+          >
+            {button.icon}
+            {button.text}
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  });
+
 const Home = () => (
   <Grid container className="home" spacing={16} justify="space-evenly">
     <Grid item xs={12}>
@@ -14,43 +33,13 @@ const Home = () => (
     </Grid>
     <Grid item xs={12}>
       <Grid container spacing={16} justify="space-evenly">
-        {HomeButtonsMajor.map((button, index) => {
-          return (
-            <Grid item xs={4} key={button.text}>
-              <Grid container direction="column" align="center">
-                <Button
-                  component={Link}
-                  to={button.to}
-                  className="button button--home"
-                  data-test={button.text}
-                >
-                  {button.icon}
-                  {button.text}
-                </Button>
-              </Grid>
-            </Grid>
-          );
-        })}
+        {renderButtons(HomeButtonsMajor, { className: 'button button--home' })}
       </Grid>
     </Grid>
     <Grid item xs={12}>
       <Grid container spacing={16} justify="space-evenly">
-        {HomeButtonsMinor.map(button => {
-          return (
-            <Grid item xs={4} key={button.text}>
-              <Grid container direction="column" align="center">
-                <Button
-                  component={Link}
-                  to={button.to}
-                  className="button button--home button--home-minor"
-                  data-test={button.text}
-                >
-                  {button.icon}
-                  {button.text}
-                </Button>
-              </Grid>
-            </Grid>
-          );
+        {renderButtons(HomeButtonsMinor, {
+          className: 'button button--home button--home-minor'
         })}
       </Grid>
     </Grid>
