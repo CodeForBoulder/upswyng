66c5f30976fecb854659fa5b57b585684e3a3c13
Jacob <jacob.venable@gmail.com>
2019-04-09 08:17:39 -0600

    Add/landing page/buttons (#44)
    * Update App test to test for finding elements
    
    * Create base <Search/> and add to home component.
    
    Create tests for both search and home components.
    
    * Place absolute imports before relative in App test
    
    * Update App test to use Jest mock.
    
    App test now looks for specific path and component pairings.
    
    * Remove wrapping element search in App test.
    
    * Update Search component test.
    
    Split separate testing rendered components into separate blocks.
    
    * Update App component route testing.
    
    Have the route testing also check the component rendered on a route.
    
    * Update Home component test to use shallow render.
    
    Split searching for proper number of layout components and layout component displayed as Search component into separate it blocks.
    
    Update Home component to search for a prop value on one Grid element since we are only using a shallow render.
    
    * Update Home component test to use shallow render.
    
    Split searching for proper number of layout components and layout component displayed as Search component into separate it blocks.
    
    Update Home component to search for a prop value on one Grid element since we are only using a shallow render.
    
    * Add buttons to the home page.
    
    * Update home buttons to use react router links.
    
    Updates home page tests to account for searching for the proper element for a home button as well as the correct props set on the element.
    
    * Update let to const in Home test
    
    * Separate home page icons into Icons module
    
    Update home test to mock the new Icons module.
    
    * Updates <Home/> test to be more intuitive
    
    Split testing properties and children of each button into separate assertions.
    
    Add the data-test attribute to buttons to make finding matching buttons easier.
    
    * Separate home button arrays into separate files.
    
    * Remove unnecessary wrapper on home component
    
    * Clean up test file
    
    * Install dependencies
    
    * Move button rendering to helper function.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { Button, Grid } from '@material-ui/core';
+import HomeButtonsMajor from './HomeButtonsMajor';
+import HomeButtonsMinor from './HomeButtonsMinor';
 import Search from './Search';
+
+const renderButtons = (buttons, props) =>
+  buttons.map((button, index) => {
+    return (
+      <Grid item xs={4} key={button.text}>
+        <Grid container direction="column" align="center">
+          <Button
+            component={Link}
+            to={button.to}
+            data-test={button.text}
+            {...props}
+          >
+            {button.icon}
+            {button.text}
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  });
 
 const Home = () => (
   <Grid container className="home" spacing={16} justify="space-evenly">
-    <Grid component={Search} item xs={12} />
+    <Grid item xs={12}>
+      <Grid container className="home" spacing={16} justify="space-evenly">
+        <Grid component={Search} item xs={12} />
+      </Grid>
+    </Grid>
+    <Grid item xs={12}>
+      <Grid container spacing={16} justify="space-evenly">
+        {renderButtons(HomeButtonsMajor, { className: 'button button--home' })}
+      </Grid>
+    </Grid>
+    <Grid item xs={12}>
+      <Grid container spacing={16} justify="space-evenly">
+        {renderButtons(HomeButtonsMinor, {
+          className: 'button button--home button--home-minor'
+        })}
+      </Grid>
+    </Grid>
   </Grid>
 );
 
--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -1,17 +1,103 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Home from './Home';
+import HomeButtonsMajor from './HomeButtonsMajor';
+import HomeButtonsMinor from './HomeButtonsMinor';
+jest.mock('@material-ui/core/Button', () => 'button-material-ui');
 jest.mock('@material-ui/core/Grid', () => 'Grid');
+jest.mock('./Icons');
 jest.mock('./Search', () => 'Search');
 
 describe('<Home/>', () => {
   const wrapper = shallow(<Home />);
+
   it('renders the proper number of layout components', () => {
-    expect(wrapper.find('Grid').length).toBe(2);
+    const numSearchGridComponents = 3;
+    const numButtonsMajorGridComponents = 2 + HomeButtonsMajor.length * 2;
+    const numButtonsMinorGridComponents = 2 + HomeButtonsMinor.length * 2;
+    expect(wrapper.find('Grid').length).toBe(
+      1 +
+        numSearchGridComponents +
+        numButtonsMajorGridComponents +
+        numButtonsMinorGridComponents
+    );
   });
+
   it('renders one layout component as the search component', () => {
     expect(
       wrapper.findWhere(n => n.prop('component') === 'Search').length
     ).toBe(1);
+  });
+
+  describe('major buttons', () => {
+    const renderedMajorButtons = wrapper.find(
+      'button-material-ui.button.button--home:not(.button--home-minor)'
+    );
+
+    it('renders the proper number of major buttons', () => {
+      expect(renderedMajorButtons.length).toBe(HomeButtonsMajor.length);
+    });
+
+    HomeButtonsMajor.forEach(expectedButton => {
+      describe(`${expectedButton.text} button`, () => {
+        const renderedButton = renderedMajorButtons.find(
+          `[data-test="${expectedButton.text}"]`
+        );
+
+        it(`renders only one ${expectedButton.text} button`, () => {
+          expect(renderedButton.length).toBe(1);
+        });
+
+        it('renders one button with matching text', () => {
+          expect(renderedButton.render().text()).toBe(expectedButton.text);
+        });
+
+        it('renders one button with a matching icon', () => {
+          expect(
+            renderedButton.containsMatchingElement(expectedButton.icon)
+          ).toBe(true);
+        });
+
+        it('renders one button with a matching link', () => {
+          expect(renderedButton.prop('to')).toBe(expectedButton.to);
+        });
+      });
+    });
+  });
+
+  describe('minor buttons', () => {
+    const renderedMinorButtons = wrapper.find(
+      'button-material-ui.button.button--home.button--home-minor'
+    );
+
+    it('renders the proper number of minor buttons', () => {
+      expect(renderedMinorButtons.length).toBe(HomeButtonsMinor.length);
+    });
+
+    HomeButtonsMinor.forEach(expectedButton => {
+      describe(`${expectedButton.text} button`, () => {
+        const renderedButton = renderedMinorButtons.find(
+          `[data-test="${expectedButton.text}"]`
+        );
+
+        it(`renders only one ${expectedButton.text} button`, () => {
+          expect(renderedButton.length).toBe(1);
+        });
+
+        it('renders one button with matching text', () => {
+          expect(renderedButton.render().text()).toBe(expectedButton.text);
+        });
+
+        it('renders one button with a matching icon', () => {
+          expect(
+            renderedButton.containsMatchingElement(expectedButton.icon)
+          ).toBe(true);
+        });
+
+        it('renders one button with a matching link', () => {
+          expect(renderedButton.prop('to')).toBe(expectedButton.to);
+        });
+      });
+    });
   });
 });
