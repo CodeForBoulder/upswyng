e8582e2adc2daa80086c58e60343d985a7c13913
Jacob <jacob.venable@gmail.com>
2019-06-05 03:01:51 -0600

    Add listing page shell component (#64)
    * Add Resource component as route.
    
    Update App test to account for an additional route.
    
    Create new, generic Resource component.
    
    Import Resource component into App and add as a route.
    
    * Add dummy data object to Resource
    
    * Add/listing page/carousel (#46)
    
    * Create initial Carousel component.
    
    Create test for Carousel that confirms it renders all passed images once.
    
    * Add handling of invalid props to Carousel.
    
    * Add IDs to mock props in Carousel test.
    
    Provide more descriptive string for handling an images prop that is not an array test.
    
    * Add key prop to map of images.
    
    * Add Carousel to Resource component.
    
    Remove unnecessary ResourceComponent and place directly in Resource.
    
    * Rename var containing singleResourceApiReturn.
    
    * Remove ID from resourceApiReturn in Carousel.
    
    Add the resource ID as a dummy variable exported from Carousel.
    
    Add images array to dummy data in Carousel.
    
    * Replace containing component w/ fragments.
    
    Clean up the return of a Carousel component.
    
    * Remove fragment from <Resource/>
    
    * convert <Resource/> to TS
    
    * move mock API returns from Resource component to Resource.test
    
    * remove <Carousel/> from <Resource/>
    
    * update Resource.test to TS
    
    * remove /resource route from <App/>
    
    * add TResource type to Resource.test
    
    * delete Carousel component
    
    * move mock resource from Resource.test to DataMocks

---
+++ src/components/Resource.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Resource from './Resource';
+import { foodResource } from '../DataMocks';
+
+describe('<Resource/>', () => {
+  const mockSingleResourceId = '-KS4rbQziVbJM5tNdEQL';
+
+  const props = {
+    id: mockSingleResourceId,
+    resource: foodResource
+  };
+  const wrapper = shallow(<Resource {...props} />);
+
+  it('renders the charityname property of the resource prop object', () => {
+    expect(wrapper.find('div').text()).toBe(props.resource.charityname);
+  });
+});
---
+++ src/components/Resource.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TResource } from '../types';
+interface Props {
+  id: string;
+  resource: TResource;
+}
+
+const Resource = (props: Props) => {
+  const {
+    resource: { charityname }
+  } = props;
+
+  return <div>{charityname}</div>;
+};
+
+export default Resource;
