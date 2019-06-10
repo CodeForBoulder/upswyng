cc5764ffc201316d0cb121855e7c09a41d8dd4d9
Jacob Venable <jacobvenable@jacobs-air.lan>
2019-06-09 21:43:45 -0600

    update getSearchParamVal() to allow returning of empty string on param without a value

--- src/utils/searchParams.ts
+++ src/utils/searchParams.ts
@@ -1,5 +1,5 @@
 export const getSearchParamVal = (paramName: string): string | null => {
   const allSearchParams = new URLSearchParams(location.search);
 
-  return allSearchParams.get(paramName) || null;
+  return allSearchParams.get(paramName);
 };
