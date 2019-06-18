a2c1eb564ac7476f7900b6d640d1c46bfd083a43
Jacob Venable <jacob.venable@gmail.com>
2019-06-17 23:42:29 -0600

    update hotline branch of DB to crisisline

--- data_pipeline/combine.js
+++ data_pipeline/combine.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
 const charity = JSON.parse(fs.readFileSync('data_pipeline/charity.json'));
-const hotline = JSON.parse(fs.readFileSync('data_pipeline/crisisline.json'));
+const crisisline = JSON.parse(fs.readFileSync('data_pipeline/crisisline.json'));
 
 const db = {
   charity,
-  hotline
+  crisisline
 };
 
 fs.writeFileSync('data_pipeline/all.json', JSON.stringify(db));
