eff29c0384dfa6bc88d9c9a547a97d430120f93a
Jacob Venable <jacob.venable@gmail.com>
2019-06-18 23:59:30 -0600

    update algolia-populate to use .env.local variables

--- data_pipeline/algolia-populate.js
+++ data_pipeline/algolia-populate.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 const algoliasearch = require('algoliasearch');
 
-dotenv.config();
+//load local environment variables
+const env = {
+  local: dotenv.parse(fs.readFileSync('.env.local'))
+};
 
 const charities = JSON.parse(
   fs.readFileSync('data_pipeline/charity.json').toString('utf8')
@@ -25,12 +28,12 @@ for (const charityID in charities) {
 
 // initialize Algolia project
 const algolia = algoliasearch(
-  process.env.ALGOLIA_APP_ID,
-  process.env.ALGOLIA_ADMIN_API_KEY
+  env.local.ALGOLIA_APP_ID,
+  env.local.ALGOLIA_ADMIN_API_KEY
 );
 
 // initialize Algolia project index
-const index = algolia.initIndex(process.env.ALGOLIA_INDEX_NAME);
+const index = algolia.initIndex(env.local.ALGOLIA_INDEX_NAME);
 
 // add/update charities
 index
