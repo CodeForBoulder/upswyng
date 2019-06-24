bc75e6e611197e0aad00da57b86ece96c919ccde
Jacob <jacob.venable@gmail.com>
2019-06-24 08:04:59 -0600

    Update <Search/> to Redirect with Query Parameter (#75)
    * update <Search/> extension to typescript
    
    * add hanldeSubmit method to <Search/> to display <Redirect/>
    
    * remove unnecessary form render test from Search.test
    
    * update <Search/> text field to be controlled component
    
    * add HTMLFormElement interface to handleSubmit event parameter in <Search/>
    
    * add SEARCH_PARAM_QUERY constant
    
    * update path of redirect when <Search/> form submitted
    
    * use shorthand react fragment tag

---
+++ src/components/Search.tsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { InputAdornment, TextField } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import { Redirect } from 'react-router';
+
+import { SEARCH_PARAM_QUERY } from '../constants';
+
+interface State {
+  query: string;
+  submitted: boolean;
+}
+
+class Search extends Component {
+  state: State = {
+    query: '',
+    submitted: false
+  };
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value }
+    } = e;
+
+    this.setState({
+      query: value
+    });
+  };
+
+  handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    this.setState({
+      submitted: true
+    });
+  };
+
+  render() {
+    const { submitted, query } = this.state;
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <TextField
+            id="search"
+            label="Search"
+            type="text"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <SearchIcon />
+                </InputAdornment>
+              )
+            }}
+            onChange={this.handleChange}
+            value={query}
+          />
+        </form>
+        {submitted ? (
+          <Redirect
+            to={{
+              pathname: '/search',
+              search: `?${SEARCH_PARAM_QUERY}=${query}`
+            }}
+          />
+        ) : null}
+      </>
+    );
+  }
+}
+
+export default Search;
