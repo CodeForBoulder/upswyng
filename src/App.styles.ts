c261cb666b053070939c0a4dc61fd501b9d85a03
Jacob <jacob.venable@gmail.com>
2019-07-07 21:53:17 -0600

    Styled Components for the Home Page (#84)
    * add styled-components as dependency
    
    * add styled-components types as dependency
    
    * create initial base styles for the app
    
    * delete unused App.css
    
    * start initial styling for app and header
    
    * create Logo component
    
    * remove unused onClick handler for menu button
    
    * move header padding into variable
    
    * wrap all <Home/> children in container
    
    * update search input styles
    
    * add ScreenReaderOnly css helper and apply to <Search/> label
    
    * add open sans font-family and apply to <Search/>
    
    * create searcn input adornment styles
    
    * change header vertical padding to margins
    
    * create aStyles css helper and apply to global styles
    
    * remove major vs minor home page buttons
    
    * move home button rendering to <HomeButtons/>
    
    * fix greyDark variable color in App.styles
    
    * update home button styles
    
    * add margins to the home buttons container
    
    * add color as a property found on HomeButton object
    
    * complete initial styling for the home page
    
    * fix Home.test
    
    * fix Search.test
    
    * change wrapping element of App to a fragment
    
    * alphabetize css properties
    
    * fix green color hex
    
    * fix menu button alignment in Header.tsx

---
+++ src/App.styles.ts
@@ -0,0 +1,63 @@
+import styled, { createGlobalStyle, css } from 'styled-components';
+import { Grid } from '@material-ui/core';
+
+export const colors = {
+  black: '#000',
+  blue: '#2C4198',
+  brown: '#AA6A03',
+  charcoal: '#1D1D1D',
+  gold: '#F4BD21',
+  green: '#68A840',
+  greyDark: '#3c3c3c',
+  greyLight: '#707070',
+  lavendar: '#9F6CBA',
+  orangeDark: '#CE5A30',
+  orangePrimary: '#F05A28',
+  orangeSecondary: '#DB5427',
+  pink: '#B146BF',
+  purple: '#5B4DC7',
+  red: '#BC2222',
+  rosewood: '#795161',
+  teal: '#40ACBF',
+  white: '#FFF'
+};
+
+export const fonts = {
+  openSans: "'Open Sans', sans-serif"
+};
+
+export const ScreenReaderOnly = css`
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: inset(50%);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+`;
+
+export const aStyles = css`
+  &:link,
+  &:visited,
+  &:hover,
+  &:active {
+    color: ${colors.white};
+  }
+`;
+
+export const Container = styled(Grid)`
+  margin: 0 auto;
+  max-width: 480px;
+` as typeof Grid;
+
+export default createGlobalStyle`
+  body {
+    background: ${colors.charcoal};
+    color: ${colors.white};
+    font-family: ${fonts.openSans};
+  }
+  a {
+    ${aStyles}
+  }
+`;
