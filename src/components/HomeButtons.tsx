e14b0889cad10aa4d3b1e2715922680a469dac07
Jacob Venable <jacob.venable@gmail.com>
2019-07-13 11:05:56 -0600

    remove unused material-ui Grid import from HomeButtons

--- src/components/HomeButtons.tsx
+++ src/components/HomeButtons.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Grid } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import { ButtonProps } from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
