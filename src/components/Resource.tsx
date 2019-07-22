2c9ac7d8835b491b616d2c0a1ec8f1d89c925813
Jacob Venable <jacob.venable@gmail.com>
2019-07-21 18:00:39 -0600

    start generateSchedule() helper

--- src/components/Resource.tsx
+++ src/components/Resource.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TResource } from '../types';
+import { TDay, TResource, TSchedule } from '../types';
 import useResource from './useResource';
 import { getSearchParamVal } from '../utils/searchParams';
 import { SEARCH_PARAM_RESOURCE, FIREBASE_RESOURCE_BRANCH } from '../constants';
@@ -7,15 +7,52 @@ import LoadingSpinner from './LoadingSpinner';
 import { Container } from '../App.styles';
 
 const renderContactInfo = (resource: TResource) => {
-  const { address1, address2, city, state, zip } = resource;
+  const { address1, address2, city, phone, state, zip } = resource;
   return (
     <>
       <h2>Address</h2>
       <p>
         {address1}, {address2 ? `${address2},` : ''} {city}, {state} {zip}
       </p>
+      <h2>Phone</h2>
+      <p>{phone}</p>
     </>
   );
+};
+
+const days: TDay[] = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+
+const orderWeeklySchedule = (schedule: TSchedule[]) => {
+  return schedule.sort(
+    ({ day: day1 }, { day: day2 }) => days.indexOf(day1) - days.indexOf(day2)
+  );
+};
+
+const generateSchedule = (schedule: TSchedule[]) => {
+  const { type } = schedule[0];
+  if (type === 'Weekly') {
+    const orderedSchedule = orderWeeklySchedule(schedule);
+  }
+};
+
+const renderHours = (schedule: TSchedule[]) => {
+  if (schedule.length) {
+    return (
+      <>
+        <h2>Hours</h2>
+        <p>stuff</p>
+      </>
+    );
+  }
+  return null;
 };
 
 export const Resource = () => {
@@ -28,7 +65,7 @@ export const Resource = () => {
     return null;
   }
 
-  const { charityname } = resource;
+  const { charityname, schedule } = resource;
 
   if (!charityname) {
     return <LoadingSpinner />;
@@ -38,6 +75,7 @@ export const Resource = () => {
     <Container>
       <h1>{charityname}</h1>
       {renderContactInfo(resource)}
+      {renderHours(schedule)}
     </Container>
   );
 };
