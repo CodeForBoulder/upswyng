dc64dd172d92d29df7e08f0a2b7114611025e423
Jacob <jacob.venable@gmail.com>
2019-07-23 06:14:28 -0600

    update <Resource/> to use withResource (#94)
    * replace use of withResource with useResource in <Resource/>
    
    * delete withResource HOC
    
    * remove props from <Shelters/>
    
    * fix failing Resource.test

--- src/components/Shelters.tsx
+++ src/components/Shelters.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import withResource from './withResource';
 import { TResource } from '../types';
 
 interface Props {
   resource: TResource;
 }
 
-const Shelters = (props: Props) => (
+const Shelters = () => (
   <div>
     <p>Shelters</p>
-    <p>{JSON.stringify(props.resource)}</p>
   </div>
 );
 
-export default withResource(Shelters, '/charity');
+export default Shelters;
