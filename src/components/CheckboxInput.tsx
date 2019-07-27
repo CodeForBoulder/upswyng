1e8f33b6e21b2bd6848342dfe029144ffa83bf85
Jacob Venable <jacob.venable@gmail.com>
2019-07-27 17:06:59 -0600

    add initial styles to CheckboxInput

--- src/components/CheckboxInput.tsx
+++ src/components/CheckboxInput.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Checkbox, FormControlLabel } from '@material-ui/core';
+import { colors, font } from '../App.styles';
 
 interface CheckboxInputProps {
   label: string;
@@ -8,8 +9,15 @@ interface CheckboxInputProps {
   value: string;
 }
 
+const StyledFormControlLabel = styled(FormControlLabel)`
+  span {
+    color: ${colors.white};
+    font-family: ${font.families.openSans};
+  }
+` as typeof FormControlLabel;
+
 const CheckboxInput = (props: CheckboxInputProps) => (
-  <FormControlLabel
+  <StyledFormControlLabel
     control={<Checkbox onChange={() => props.onChange()} value={props.value} />}
     label={props.label}
   />
