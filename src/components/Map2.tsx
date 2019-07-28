2fc23e8c2e268a9d0652c11367e89926089c4de8
Jacob Venable <jacob.venable@gmail.com>
2019-07-27 18:30:36 -0600

    start new Map component

---
+++ src/components/Map2.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Map = () => {
+  const [googleMap, setGoogleMap] = useState(null);
+  const [googleMaps, setGoogleMaps] = useState(null);
+  const [directionsDisplay, setDirectionsDisplay] = useState(null);
+  const [directionsService, setDirectionsService] = useState(null);
+  const [directionsAreToggled, setDirectionsAreToggled] = useState(false);
+
+  return <div />;
+};
