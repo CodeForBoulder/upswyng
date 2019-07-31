927454cb687e9a910ed62b383116dab73c54040d
Jacob Venable <jacob.venable@gmail.com>
2019-07-31 08:23:26 -0600

    move fetch status types to enum in types.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,3 @@ export const SEARCH_PARAM_RESOURCE = 'id';
 export const SEARCH_PARAM_QUERY = 'q';
 
 export const FIREBASE_RESOURCE_BRANCH = '/charity';
-
-export const STATUS_NOT_FETCHED = 'NOT_FETCHED';
-export const STATUS_FETCHING = 'FETCHING';
-export const STATUS_FETCHED = 'FETCHED';
-export const STATUS_ERROR = 'ERROR';
