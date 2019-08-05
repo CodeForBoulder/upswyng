6eb7ae9c359f4de961242e1668d4d57d4790d705
Jacob Venable <jacob.venable@gmail.com>
2019-08-04 22:05:45 -0600

    add results to <Food/> page

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Container } from '../App.styles';
+import useSimilarSearchResults from './useSimilarSearchResults';
+import SearchResults from './SearchResults';
 
-const Food = () => (
-  <div>
-    <p>Food</p>
-  </div>
-);
+const Food = () => {
+  const searchQuery = 'food';
+  const searchResults = useSimilarSearchResults(searchQuery);
+  return (
+    <Container>
+      <h1>Food</h1>
+      <SearchResults results={searchResults} />
+    </Container>
+  );
+};
 
 export default Food;
