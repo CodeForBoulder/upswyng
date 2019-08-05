bade51b8e17b98d7b151ca6f5734c2eb321b96b8
Jacob Venable <jacob.venable@gmail.com>
2019-08-04 22:17:01 -0600

    create <Health/> page

---
+++ src/components/Health.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from '../App.styles';
+import useSimilarSearchResults from './useSimilarSearchResults';
+import SearchResults from './SearchResults';
+
+const Health = () => {
+  const searchQuery = 'health';
+  const searchResults = useSimilarSearchResults(searchQuery);
+  return (
+    <Container>
+      <h1>Health</h1>
+      <SearchResults results={searchResults} />
+    </Container>
+  );
+};
+
+export default Health;
