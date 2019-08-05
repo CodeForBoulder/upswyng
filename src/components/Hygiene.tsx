5d208a8e9f05a2c0448fa57d6db6fe6f2cfc5578
Jacob Venable <jacob.venable@gmail.com>
2019-08-04 21:59:00 -0600

    add <SearchResults/> to Hygiene page

--- src/components/Hygiene.tsx
+++ src/components/Hygiene.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Container } from '../App.styles';
+import useSimilarSearchResults from './useSimilarSearchResults';
+import LoadingSpinner from './LoadingSpinner';
+import SearchResults from './SearchResults';
 
-const Hygiene = () => (
-  <div>
-    <p>Hygiene</p>
-  </div>
-);
+const Hygiene = () => {
+  const searchQuery = 'hygiene';
+  const searchResults = useSimilarSearchResults(searchQuery);
+  return (
+    <Container>
+      <h1>Hygiene</h1>
+      {!searchResults && <LoadingSpinner />}
+      {searchResults && <SearchResults results={searchResults} />}
+    </Container>
+  );
+};
 
 export default Hygiene;
