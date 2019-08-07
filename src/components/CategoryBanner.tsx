4167e973ef681547c608cca902cf9e2d23308e01
Jacob Venable <jacob.venable@gmail.com>
2019-08-07 01:43:56 -0600

    add <Link/> to home page in <CategoryBanner/>

--- src/components/CategoryBanner.tsx
+++ src/components/CategoryBanner.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface Props {
   text: string;
 }
 
-const CategoryBanner = ({ text }: Props) => <h1>{text}</h1>;
+const CategoryBanner = ({ text }: Props) => (
+  <Link to="/">
+    <h1>{text}</h1>
+  </Link>
+);
 
 export default CategoryBanner;
