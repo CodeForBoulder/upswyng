a4f89d3c0cea1bc765ed2bc69a0684ebf1beefd4
Jacob Venable <jacob.venable@gmail.com>
2019-08-06 23:04:40 -0600

    update JobTraining category query

--- src/components/JobTraining.tsx
+++ src/components/JobTraining.tsx
@@ -4,7 +4,8 @@ import useSimilarSearchResults from './useSimilarSearchResults';
 import SearchResults from './SearchResults';
 
 const JobTraining = () => {
-  const searchQuery = 'job, career';
+  const searchQuery =
+    'job, career, financial aid, GED, school, K-12, library, vocational, training';
   const searchResults = useSimilarSearchResults(searchQuery);
   return (
     <Container>
