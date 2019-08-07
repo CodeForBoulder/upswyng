4243eaa2001c4ff9793b8e0366b4a60d6bc12ecf
Jacob Venable <jacob.venable@gmail.com>
2019-08-07 03:42:11 -0600

    add styles for image container and resource name in <ResourceCard/>

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -1,22 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import { SEARCH_PARAM_RESOURCE } from '../constants';
+import { colors, font } from '../App.styles';
 
 interface Props {
   resourceId: string;
   resourceName: string;
 }
 
+const ResourceCardContainer = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 auto;
+  font-family: ${font.families.openSans};
+`;
+
+const ResourceCardImageContainer = styled.div`
+  position: relative;
+  background: ${colors.greyLight};
+  flex: 1 1 auto;
+  &::before {
+    content: '';
+    display: block;
+    padding-bottom: ${(96 / 146) * 100}%;
+    width: 100%;
+  }
+`;
+
+const ResourceCardResourceName = styled.span`
+  bottom: 4px;
+  color: ${colors.white};
+  display: flex;
+  flex-direction: column;
+  font-shadow: 
+  font-size: ${font.helpers.convertPixelsToRems(14)};
+  font-weight: 700;
+  padding: 0 8px 8px;
+  position: absolute;
+  width: 100%;
+`;
+
 const ResourceCard = ({ resourceId, resourceName }: Props) => (
-  <Link
+  <ResourceCardContainer
     to={{
       pathname: '/resource',
       search: `?${SEARCH_PARAM_RESOURCE}=${resourceId}`
     }}
   >
-    <img src={'https://via.placeholder.com/150'} alt={'placeholder'} />
-    {resourceName}
-  </Link>
+    <ResourceCardImageContainer>
+      <ResourceCardResourceName>{resourceName}</ResourceCardResourceName>
+    </ResourceCardImageContainer>
+  </ResourceCardContainer>
 );
 
 export default ResourceCard;
