41419bcfa41d0ec7818729d7880aa8e6b356ffce
Jacob Venable <jacob.venable@gmail.com>
2019-08-10 15:57:06 -0600

    add initial subcategory rendering to health page

--- src/components/Health.tsx
+++ src/components/Health.tsx
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TSubCategory } from '../types';
 import { colors, Container } from '../App.styles';
 import useSearchResults from './useSearchResults';
 import CategoryBanner from './CategoryBanner';
 import SearchResults from './SearchResults';
 
 const Health = () => {
-  const searchQuery = 'CATEGORY-health';
+  const [searchQuery, updateSearchQuery] = useState('CATEGORY-health');
   const searchResults = useSearchResults(searchQuery);
+  const subCategories: TSubCategory[] = [
+    {
+      text: 'Addiction Recovery Services',
+      query: 'SUBCATEGORY-health-addictionRecoveryServices'
+    },
+    {
+      text: 'Clinics',
+      query: 'SUBCATEGORY-health-clinic'
+    },
+    {
+      text: 'Dental',
+      query: 'SUBCATEGORY-health-dental'
+    },
+    {
+      text: 'Hospital',
+      query: 'SUBCATEGORY-health-hospital'
+    },
+    {
+      text: 'Mental',
+      query: 'SUBCATEGORY-health-mental'
+    },
+    {
+      text: 'Pharmacies',
+      query: 'SUBCATEGORY-health-pharmacy'
+    },
+    {
+      text: 'Vision',
+      query: 'SUBCATEGORY-health-vision'
+    }
+  ];
+
+  const handleSubCategoryClick = (query: string) => {
+    updateSearchQuery(query);
+  };
+
+  const renderSubCategories = (subCategories: TSubCategory[]) => (
+    <ul>
+      {subCategories.map(({ text, query }) => (
+        <li key={query} onClick={() => handleSubCategoryClick(query)}>
+          {text}
+        </li>
+      ))}
+    </ul>
+  );
+
   return (
     <Container>
       <CategoryBanner text="Health" color={colors.red} />
+      {renderSubCategories(subCategories)}
       <SearchResults results={searchResults} />
     </Container>
   );
