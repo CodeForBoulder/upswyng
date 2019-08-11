7b681f36715d03b5747a5d22bb5b57c8e7d2b784
Jacob Venable <jacob.venable@gmail.com>
2019-08-11 15:40:06 -0600

    add <Shelters/> sub-categories

--- src/components/Shelters.tsx
+++ src/components/Shelters.tsx
@@ -1,18 +1,52 @@
 import React from 'react';
+import { TResourceCategory } from '../types';
 import { colors, Container } from '../App.styles';
-import useSearchResults from './useSearchResults';
-import CategoryBanner from './CategoryBanner';
-import SearchResults from './SearchResults';
+import CategoryResults from './CategoryResults';
 
-const Shelters = () => {
-  const searchQuery = 'CATEGORY-shelter';
-  const searchResults = useSearchResults(searchQuery);
-  return (
-    <Container>
-      <CategoryBanner text="Shelters" color={colors.orangeDark} />
-      <SearchResults results={searchResults} />
-    </Container>
-  );
+const categoryColor = colors.orangePrimary;
+const category: TResourceCategory = {
+  text: 'Shelters',
+  query: 'CATEGORY-shelter'
 };
+const subCategories: TResourceCategory[] = [
+  {
+    text: 'Pregnant',
+    query: 'SUBCATEGORY-shelter-pregnant'
+  },
+  {
+    text: 'Abused',
+    query: 'SUBCATEGORY-shelter-abused'
+  },
+  {
+    text: 'Youth',
+    query: 'SUBCATEGORY-shelter-youth'
+  },
+  {
+    text: 'Family',
+    query: 'SUBCATEGORY-shelter-family'
+  },
+  {
+    text: 'Transitional',
+    query: 'SUBCATEGORY-shelter-transitional'
+  },
+  {
+    text: 'Temporary',
+    query: 'SUBCATEGORY-shelter-temporary'
+  },
+  {
+    text: 'Emergency',
+    query: 'SUBCATEGORY-shelter-emergency'
+  }
+];
+
+const Shelters = () => (
+  <Container>
+    <CategoryResults
+      category={category}
+      color={categoryColor}
+      subCategories={subCategories}
+    />
+  </Container>
+);
 
 export default Shelters;
