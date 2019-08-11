2e54d06d9c3f4f319c0503cb2bead48d89298b5c
Jacob Venable <jacob.venable@gmail.com>
2019-08-11 15:30:07 -0600

    add <Resources/> sub-categories

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -1,18 +1,56 @@
 import React from 'react';
+import { TResourceCategory } from '../types';
 import { colors, Container } from '../App.styles';
-import useSearchResults from './useSearchResults';
-import CategoryBanner from './CategoryBanner';
-import SearchResults from './SearchResults';
+import CategoryResults from './CategoryResults';
 
-const Resources = () => {
-  const searchQuery = 'CATEGORY-resources';
-  const searchResults = useSearchResults(searchQuery);
-  return (
-    <Container>
-      <CategoryBanner text="Resources" color={colors.purple} />
-      <SearchResults results={searchResults} />
-    </Container>
-  );
+const categoryColor = colors.purple;
+const category: TResourceCategory = {
+  text: 'Resources',
+  query: 'CATEGORY-resources'
 };
+const subCategories: TResourceCategory[] = [
+  {
+    text: 'Pets',
+    query: 'SUBCATEGORY-resources-pets'
+  },
+  {
+    text: 'Hair Care',
+    query: 'SUBCATEGORY-resources-hairCare'
+  },
+  {
+    text: 'Laundry',
+    query: 'SUBCATEGORY-resources-laundry'
+  },
+  {
+    text: 'Legal Help',
+    query: 'SUBCATEGORY-resources-legalHelp'
+  },
+  {
+    text: 'Outdoor Gear',
+    query: 'SUBCATEGORY-resources-outdoorGear'
+  },
+  {
+    text: 'Home Goods',
+    query: 'SUBCATEGORY-resources-homeGoods'
+  },
+  {
+    text: 'Shoes',
+    query: 'SUBCATEGORY-resources-shoes'
+  },
+  {
+    text: 'Clothing',
+    query: 'SUBCATEGORY-resources-clothing'
+  }
+];
+
+const Resources = () => (
+  <Container>
+    <CategoryResults
+      category={category}
+      color={categoryColor}
+      subCategories={subCategories}
+    />
+  </Container>
+);
 
 export default Resources;
