3fc43bd4cedcf415250fbe224d8d5fa4fa3f4187
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2019-09-19 21:41:40 -0600

    💄 Fix up resource page

--- packages/upswyng-data-pipeline/server/src/routes/resource/[id].svelte
+++ packages/upswyng-data-pipeline/server/src/routes/resource/[id].svelte
@@ -28,17 +28,18 @@
   }
 
   const resourceForm = svelteForm(() => ({
-    name: { value: resource.name, validators: ["required", "min:6"] },
     // address
     address1: {
       value: resource.address.address1 || "",
       validators: ["required"]
     },
-    address2: { value: resource.address.address2 || "" },
+    address2: { value: resource.address.address2 || "", validators: [] },
     city: { value: resource.address.city, validators: ["required"] },
     state: { value: resource.address.state, validators: ["required"] },
     zip: { value: resource.address.zip, validators: ["required"] },
     //
+    closeSchedule: { value: resource.closeSchedule || [], validators: [] },
+    description: { value: resource.description || "", validators: ["min:12"] },
     latitude: {
       value: resource.latitude || 40.01,
       validators: ["between:-90:90"]
@@ -47,21 +48,17 @@
       value: resource.longitude || -105.27,
       validators: ["between:-180:180"]
     },
-    closeSchedule: { value: resource.closeSchedule || [], validators: [] },
+    name: { value: resource.name, validators: ["required", "min:6"] },
     phone: { value: resource.phone || "", validators: ["required", "min:10"] },
-    schedule: { value: resource.schedule || ["min:1"] },
+    schedule: { value: resource.schedule || [] },
     services: { value: resource.services || [], validators: [] },
-    website: { value: resource.website || "", validators: ["required", "url"] }
+    website: { value: resource.website || "", validators: ["url"] }
   }));
 </script>
 
 <svelte:head>
   <title>Upswyng: {resource.name}</title>
 </svelte:head>
-
-<section>
-  <p>{JSON.stringify($resourceForm, null, 2)}</p>
-</section>
 
 <h1>{resource.name}</h1>
 <div class="content">
@@ -70,101 +67,127 @@
   <p>Kudos: {resource.kudos}</p>
   <p>Created At: {resource.createdAt}</p>
   <p>Last Modified At: {resource.lastModifiedAt}</p>
+  <form>
+    <p>
+      <label for="name">Resource Name</label>
+      <input name="name" type="text" bind:value={resource.name} />
+      {@debug $resourceForm}
+      {#if $resourceForm.name.errors.includes('required')}
+        <p>The name is required</p>
+      {/if}
+
+      {#if $resourceForm.name.errors.includes('min')}
+        <p>The name should be at least 6 characters</p>
+      {/if}
+    </p>
+    <p>
+      <label for="description">Description</label>
+      <textarea
+        name="description"
+        type="text"
+        bind:value={resource.description} />
+      {#if $resourceForm.description.errors.includes('min')}
+        <p>The description should be at least 12 characters</p>
+      {/if}
+    </p>
+    <p>
+      <label for="phone">Phone</label>
+      <input
+        name="phone"
+        autocomplete="tel"
+        type="text"
+        bind:value={resource.phone} />
+      {#if $resourceForm.phone.errors.includes('min')}
+        <p>The phone number should be at least 10 characters</p>
+      {/if}
+    </p>
+    <fieldset>
+      <p>
+        <label for="address1">Address 1</label>
+        <input
+          name="address1"
+          autocomplete="address-line1"
+          type="text"
+          bind:value={resource.address.address1} />
+        {#if $resourceForm.address1.errors.includes('required')}
+          <p>An address is required.</p>
+        {/if}
+      </p>
+      <p>
+        <label for="address2">Address 2</label>
+        <input
+          name="address2"
+          autocomplete="address-line2"
+          type="text"
+          bind:value={resource.address.address2} />
+      </p>
+      <p>
+        <label for="city">City</label>
+        <input
+          name="city"
+          autocomplete="address-level2"
+          type="text"
+          bind:value={resource.address.city} />
+        {#if $resourceForm.city.errors.includes('required')}
+          <p>A city is required.</p>
+        {/if}
+      </p>
+      <p>
+        <label for="state">State</label>
+        <input
+          name="state"
+          autocomplete="address-level1"
+          type="text"
+          bind:value={resource.address.state} />
+        {#if $resourceForm.state.errors.includes('required')}
+          <p>A state is required.</p>
+        {/if}
+      </p>
+      <p>
+        <label for="zip">ZIP</label>
+        <input
+          name="zip"
+          autocomplete="postal-code"
+          type="text"
+          bind:value={resource.address.zip} />
+        {#if $resourceForm.zip.errors.includes('required')}
+          <p>A ZIP is required.</p>
+        {/if}
+      </p>
+    </fieldset>
+    <p>
+      <ScheduleInput bind:value={resource.schedule} />
+    </p>
+    <p>
+      <CloseScheduleInput bind:value={resource.closeSchedule} />
+    </p>
+    <p>
+      <ServicesInput bind:value={resource.services} />
+    </p>
+    <p>
+      <label for="latitude">Latitude</label>
+      <input name="latitude" type="latitude" bind:value={resource.latitude} />
+      {#if $resourceForm.latitude.errors.length}
+        <p>Invalid Latitude.</p>
+      {/if}
+      <label for="longitude">Longitude</label>
+      <input
+        name="longitude"
+        type="longitude"
+        bind:value={resource.longitude} />
+      {#if $resourceForm.longitude.errors.length}
+        <p>Invalid Longitude.</p>
+      {/if}
+    </p>
+    <p>
+      <label for="website">Website</label>
+      <input name="website" type="url" bind:value={resource.website} />
+      {#if $resourceForm.website.errors.includes('url')}
+        <p>Enter a valid URL.</p>
+      {/if}
+    </p>
+    <p>
+      <button disabled={!$resourceForm.valid}>Login</button>
+    </p>
+  </form>
 </div>
-
-<div class="content" />
-
-<form>
-  <p>
-    <label for="name">Resource Name</label>
-    <input name="name" type="text" bind:value={resource.name} />
-    {#if $resourceForm.name.errors.includes('required')}
-      <p>The name is required</p>
-    {/if}
-
-    {#if $resourceForm.name.errors.includes('min')}
-      <p>The name should be at least 6 characters</p>
-    {/if}
-  </p>
-  <p>
-    <label for="description">Description</label>
-    <textarea
-      name="description"
-      type="text"
-      bind:value={resource.description} />
-  </p>
-  <p>
-    <label for="phone">Phone</label>
-    <input
-      name="phone"
-      autocomplete="tel"
-      type="text"
-      bind:value={resource.phone} />
-  </p>
-  <fieldset>
-    <p>
-      <label for="address1">Address 1</label>
-      <input
-        name="address1"
-        autocomplete="address-line1"
-        type="text"
-        bind:value={resource.address.address1} />
-    </p>
-    <p>
-      <label for="address2">Address 2</label>
-      <input
-        name="address2"
-        autocomplete="address-line2"
-        type="text"
-        bind:value={resource.address.address2} />
-    </p>
-    <p>
-      <label for="city">City</label>
-      <input
-        name="city"
-        autocomplete="address-level2"
-        type="text"
-        bind:value={resource.address.city} />
-    </p>
-    <p>
-      <label for="state">State</label>
-      <input
-        name="state"
-        autocomplete="address-level1"
-        type="text"
-        bind:value={resource.address.state} />
-    </p>
-    <p>
-      <label for="zip">ZIP</label>
-      <input
-        name="zip"
-        autocomplete="postal-code"
-        type="text"
-        bind:value={resource.address.zip} />
-    </p>
-  </fieldset>
-  <p>
-    <ScheduleInput bind:value={resource.schedule} />
-  </p>
-  <p>
-    <CloseScheduleInput bind:value={resource.closeSchedule} />
-  </p>
-  <p>
-    <ServicesInput bind:value={resource.services} />
-  </p>
-  <p>
-    <label for="latitude">Latitude</label>
-    <input name="latitude" type="latitude" bind:value={resource.latitude} />
-
-    <label for="longitude">Longitude</label>
-    <input name="longitude" type="longitude" bind:value={resource.longitude} />
-  </p>
-  <p>
-    <label for="website">Website</label>
-    <input name="website" type="url" bind:value={resource.website} />
-  </p>
-  <p>
-    <span>{JSON.stringify(extractErrors($resourceForm), null, 2)}</span>
-    <button disabled={!$resourceForm.valid}>Login</button>
-  </p>
-</form>
