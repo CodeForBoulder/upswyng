f4cb7b46514468c97eebb0bd7e819ced22b3beed
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2019-09-23 01:01:33 -0600

    🔥 Remove sapper borat boilerplate

--- packages/upswyng-data-pipeline/server/src/routes/index.svelte
+++ packages/upswyng-data-pipeline/server/src/routes/index.svelte
@@ -7,59 +7,15 @@
 <script>
   import { stores } from "@sapper/app";
   const { session } = stores();
-//   console.log("SESSION", session);
+  //   console.log("SESSION", session);
   export let grant;
 </script>
 
-<style>
-  h1,
-  figure,
-  p {
-    text-align: center;
-    margin: 0 auto;
-  }
-
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
-  }
-</style>
-
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>Upswyng</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Upswyng</h1>
 
-<figure>
-  <img alt="Borat" src="great-success.png" />
-  <figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p>
-  <strong>
-    Try editing this file (src/routes/index.svelte) to test live reloading.
-  </strong>
-</p>
+<p>Visit us in the Play Store</p>
+<p>Visit us on the Web</p>
