8ed9333880e3d0a4d2ca9f0f8870a78b92861ad0
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2019-09-22 23:59:40 -0600

    ✨ Create resource display

---
+++ packages/upswyng-data-pipeline/server/src/components/ResourceDisplay.svelte
@@ -0,0 +1,96 @@
+<script>
+  export let resource; // TResource
+
+  function displayScheduleEntry(scheduleEntry /*: TSchedule */) /*: string */ {
+    const { day, date, period, from, to, scheduleType } = scheduleEntry;
+    switch (scheduleType) {
+      case "Weekly":
+        return `<span>${day} ${from} - ${to}<span>Weekly</span></span>`;
+      case "Monthly":
+        return `<span>${day} ${from} - ${to} ${period}<span>Monthly</span></span>`;
+      case "Open 24/7":
+        return `<span>Open 24/7</span>`;
+      case "Date Range":
+        return `<span>${date} ${from} - ${to}<span>Date Range</span></span>`;
+      default:
+        throw new Error(`Unexpected schedule type: ${scheduleType}`);
+    }
+  }
+</script>
+
+<style>
+  .bullet {
+    display: inline-block;
+    width: 0.75em;
+    height: 0.75em;
+    border-radius: 100%;
+  }
+</style>
+
+<h1>{resource.name}</h1>
+<div class="content">
+  <h2>ID</h2>
+  <p>{resource.id}</p>
+  <h2>Legacy ID</h2>
+  <p>{resource.legacyId}</p>
+  <h2>Kudos</h2>
+  <p>{resource.kudos}</p>
+  <h2>Created At</h2>
+  <p>{resource.createdAt}</p>
+  <h2>Last Modified At</h2>
+  <p>{resource.lastModifiedAt}</p>
+  <h2>Description</h2>
+  <p>{resource.description}</p>
+  <h2>Phone</h2>
+  <p>{resource.phone}</p>
+  <h2>Address</h2>
+  <h3>Address 1</h3>
+  <p>{resource.address.address1}</p>
+  <h3>Address 2</h3>
+  <p>{resource.address.address2}</p>
+  <h3>City</h3>
+  <p>{resource.address.city}</p>
+  <h3>State</h3>
+  <p>{resource.address.state}</p>
+  <h3>ZIP</h3>
+  <p>{resource.address.zip}</p>
+  <h2>Services</h2>
+  <ul>
+    {#each resource.services as service}
+      <li>{service}</li>
+    {/each}
+  </ul>
+  <h2>Schedule</h2>
+  <ul>
+    {#each resource.schedule as scheduleEntry}
+      <li>
+        {@html displayScheduleEntry(scheduleEntry)}
+      </li>
+    {/each}
+  </ul>
+  <h2>Close Schedule</h2>
+  <ul>
+    {#each resource.closeSchedule as closeScheduleEntry}
+      <li>
+        {@html displayScheduleEntry(closeScheduleEntry)}
+      </li>
+    {/each}
+  </ul>
+  <h2>Latitude</h2>
+  <p>{resource.latitude}</p>
+  <h2>Longitude</h2>
+  <p>{resource.longitude}</p>
+  <h2>Subcategories</h2>
+  <ul>
+    {#each resource.subcategories as subcategory}
+      <li>
+        <div
+          class="bullet"
+          style={`background-color: ${subcategory.parentCategory.color || 'gray'}`} />
+        {subcategory.parentCategory.name} | {subcategory.name}
+      </li>
+    {/each}
+  </ul>
+  <h2>Website</h2>
+  <p>{resource.website}</p>
+</div>
--- packages/upswyng-data-pipeline/server/src/routes/resource/draft/[id].svelte
+++ packages/upswyng-data-pipeline/server/src/routes/resource/draft/[id].svelte
@@ -3,15 +3,33 @@
     const resourceResponse = await this.fetch(
       `/api/resource/draft/${params.id}`
     );
-    console.log(resourceResponse);
     const resourceData = await resourceResponse.json();
 
     if (resourceResponse.status !== 200) {
-      console.log(resourceResponse);
       this.error(resourceResponse.status, resourceData.message);
     } else {
-      console.log(resourceData);
-      return { draftResource: resourceData.draftResource };
+      // see if we have an existing resource corresponding to this draft
+      const existingResourceResponse = await this.fetch(
+        `/api/resource/${resourceData.draftResource.id}`
+      );
+      if (existingResourceResponse.status === 404) {
+        return {
+          draftResource: resourceData.draftResource,
+          existingResource: null
+        };
+      }
+      const existingResourceData = await existingResourceResponse.json();
+      if (existingResourceResponse.status !== 200) {
+        this.error(
+          existingResourceResponse.status,
+          existingResourceData.message
+        );
+      } else {
+        return {
+          draftResource: resourceData.draftResource,
+          existingResource: existingResourceData.resource
+        };
+      }
     }
   }
 </script>
@@ -20,17 +38,23 @@
   import { form as svelteForm } from "svelte-forms";
   import CloseScheduleInput from "../../../components/CloseScheduleInput.svelte";
   import devalue from "devalue";
+  import ResourceDisplay from "../../../components/ResourceDisplay.svelte";
   import ScheduleInput from "../../../components/ScheduleInput.svelte";
   import ServicesInput from "../../../components/ServicesInput.svelte";
   import SubcategoryInput from "../../../components/SubcategoryInput.svelte";
 
   export let draftResource;
+  export let existingResource; // resource in the directory which this draft would update; null for new resources
 </script>
 
 <svelte:head>
   <title>Upswyng: {draftResource.name} [draft]</title>
 </svelte:head>
 
-<div>
-  <p>{devalue(draftResource)}</p>
-</div>
+{#if existingResource}
+  <p>Updating Resource</p>
+{:else}
+  <p>New Resource</p>
+{/if}
+
+<ResourceDisplay resource={draftResource} />
