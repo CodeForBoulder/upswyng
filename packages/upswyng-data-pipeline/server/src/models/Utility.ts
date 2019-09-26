42c6803e2fd4d9127bf8a9fece5dd2106643c5bc
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2019-09-25 23:00:55 -0600

    ✨ Create removeResourceFromCategory utility function

--- packages/upswyng-data-pipeline/server/src/models/Utility.ts
+++ packages/upswyng-data-pipeline/server/src/models/Utility.ts
@@ -51,6 +51,56 @@ export async function addResourceToSubcategory(
   }
 }
 
+export async function removeResourceFromSubcategory(
+  resourceId: ObjectId /* Resource ObjectId */,
+  subcategoryId: ObjectId /* Subcategory ObjectId */
+) {
+  const resource = await Resource.findById(resourceId);
+  if (!resource)
+    throw new Error(`Could not find Resource with ID ${resourceId}`);
+
+  const subcategory = await Subcategory.findById(subcategoryId);
+  if (!subcategory)
+    throw new Error(`Could not find Subcategory with ID ${subcategoryId}`);
+
+  const transaction = new Transaction();
+  let runUpdate = false;
+  if (
+    resource.subcategories
+      .map((s: ObjectId) => s.toHexString())
+      .includes(subcategoryId.toHexString())
+  ) {
+    runUpdate = true;
+    transaction.update(Resource, resourceId, {
+      subcategories: resource.subcategories.filter(
+        s => s.toHexString() !== subcategoryId.toHexString()
+      )
+    });
+  }
+  if (
+    subcategory.resources
+      .map(s => s.toHexString())
+      .includes(resourceId.toHexString())
+  ) {
+    runUpdate = true;
+    transaction.update(Resource, resourceId, {
+      resources: subcategory.resources.filter(
+        r => r.toHexString() !== resourceId.toHexString()
+      )
+    });
+  }
+  if (runUpdate) {
+    try {
+      return await transaction.run();
+    } catch (error) {
+      console.error(error);
+      const rollbackObj = await transaction.rollback().catch(console.error);
+      transaction.clean();
+      return rollbackObj;
+    }
+  }
+}
+
 /**
  * Takes two resources and removes the fields that are the same on each.
  */
