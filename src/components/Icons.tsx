0e172b548388942b9702c4379972d68ca0d89dc6
jacobvenable <jacob.venable@gmail.com>
2019-10-15 20:29:38 -0600

    add menu icons to icons list

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -8,7 +8,9 @@ import {
   DirectionsCar,
   DirectionsWalk,
   Home,
+  Info,
   LocalHospital,
+  NextWeek,
   People,
   Pets,
   Wifi
@@ -31,8 +33,12 @@ export const CarIcon: React.ReactElement<SvgIconProps> = <DirectionsCar />;
 export const CloseIcon: React.ReactElement<SvgIconProps> = <Close />;
 export const HomeIcon: React.ReactElement<SvgIconProps> = <Home />;
 export const HygieneIcon: React.ReactElement<SvgIconProps> = <Hygiene />;
+export const InfoIcon: React.ReactElement<SvgIconProps> = <Info />;
 export const LocalHospitalIcon: React.ReactElement<SvgIconProps> = (
   <LocalHospital />
+);
+export const TermsOfServiceIcon: React.ReactElement<SvgIconProps> = (
+  <NextWeek />
 );
 export const PeopleIcon: React.ReactElement<SvgIconProps> = <People />;
 export const PetsIcon: React.ReactElement<SvgIconProps> = <Pets />;
