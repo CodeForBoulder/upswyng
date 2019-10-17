77df2b61262e7d27fc86d7d1861118090af82be2
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2019-10-16 20:29:05 -0600

    ✨ Add sync to algolia

--- packages/upswyng-data-pipeline/data-pipeline.rollup.config.js
+++ packages/upswyng-data-pipeline/data-pipeline.rollup.config.js
@@ -3,23 +3,13 @@ import json from "rollup-plugin-json";
 import pkg from "./package.json";
 import typescript from "rollup-plugin-typescript2";
 
-export default [
-  {
-    input: "server/data_pipeline/syncStrappd.ts",
-    output: { file: "__build__/syncStrappd.js", format: "cjs" },
-    plugins: [commonjs(), typescript(), json()],
-    external: Object.keys(pkg.dependencies || {}).concat(
-      require("module").builtinModules ||
-        Object.keys(process.binding("natives"))
-    )
-  },
-  {
-    input: "server/data_pipeline/setupCategories.ts",
-    output: { file: "__build__/setupCategories.js", format: "cjs" },
-    plugins: [commonjs(), typescript(), json()],
-    external: Object.keys(pkg.dependencies || {}).concat(
-      require("module").builtinModules ||
-        Object.keys(process.binding("natives"))
-    )
-  }
-];
+const targets = ["syncStrappd", "updateAlgolia", "setupCategories"];
+
+export default targets.map(target => ({
+  input: `server/data_pipeline/${target}.ts`,
+  output: { file: `__build__/${target}.js`, format: "cjs" },
+  plugins: [commonjs(), typescript(), json()],
+  external: Object.keys(pkg.dependencies || {}).concat(
+    require("module").builtinModules || Object.keys(process.binding("natives"))
+  )
+}));
