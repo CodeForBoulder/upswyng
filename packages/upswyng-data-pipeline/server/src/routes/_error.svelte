98204a888a6e9b0bb500acdeddc4c7126bb55bcf
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2019-10-17 23:11:20 -0600

    💄 Redo error page layout with balena

--- packages/upswyng-data-pipeline/server/src/routes/_error.svelte
+++ packages/upswyng-data-pipeline/server/src/routes/_error.svelte
@@ -1,40 +1,21 @@
 <script>
-	export let status;
-	export let error;
+  export let status;
+  export let error;
 
-	const dev = process.env.NODE_ENV === 'development';
+  const dev = process.env.NODE_ENV === "development";
 </script>
 
-<style>
-	h1, p {
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>{status}</title>
+  <title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
+<section class="section">
+  <div class="container">
+    <h1 class="title">{status}</h1>
+    <p class="subtitle">{error.message}</p>
 
-<p>{error.message}</p>
-
-{#if dev && error.stack}
-	<pre>{error.stack}</pre>
-{/if}
+    {#if dev && error.stack}
+      <pre>{error.stack}</pre>
+    {/if}
+  </div>
+</section>
