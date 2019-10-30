93152dd7d2173125058963dd722216a46e97711f
Jacob <jacob.venable@gmail.com>
2019-10-30 01:30:55 -0600

    Add google tag manager (#148)
    * add GTM env variables
    
    * add react-gtm-module as dependency
    
    * add GTM env to types
    
    * initialize GTM in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TagManager from 'react-gtm-module';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { TEnvVariables } from './types';
+
+declare const process: TEnvVariables;
+
+const tagManagerArgs = {
+  gtmId: process.env.REACT_APP_GOOGLE_TAG_MANAGER_CONTAINER_ID,
+  auth: process.env.REACT_APP_GOOGLE_TAG_MANAGER_AUTH,
+  preview: process.env.REACT_APP_GOOGLE_TAG_MANAGER_PREVIEW
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
