7f6a2bc00d513e496b3b08df5d3f70989a8b6843
jacobvenable <jacob.venable@gmail.com>
2019-11-05 18:05:12 -0700

    remove old client-related files from top dir

--- src/components/Hotlines.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Hotlines = () => (
-  <div>
-    <p>Hotlines</p>
-  </div>
-);
-
-export default Hotlines;
--- src/utils/searchParams.ts
+++
@@ -1,5 +0,0 @@
-export const getSearchParamVal = (paramName: string): string | null => {
-  const allSearchParams = new URLSearchParams(location.search);
-
-  return allSearchParams.get(paramName);
-};
