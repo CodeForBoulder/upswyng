6add9514fc06d9b9164e521d2e0f3739c8d4be4f
Jacob <jacob.venable@gmail.com>
2019-11-05 19:01:23 -0700

    cleanup top level directory (#154)
    * remove old client-related files from top dir
    
    * sync react and react-dom versions

--- src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const SEARCH_PARAM_RESOURCE = 'id';
-export const SEARCH_PARAM_QUERY = 'q';
--- src/utils/apiClient.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const apiClient = axios.create({
-  baseURL: 'https://upswyng-server.herokuapp.com/api/',
-  timeout: 10000
-});
-
-export default apiClient;
