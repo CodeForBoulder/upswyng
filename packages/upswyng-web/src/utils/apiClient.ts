c826355551e8071a474cc908291591035ab1dcac
jacobvenable <jacob.venable@gmail.com>
2019-11-05 19:47:49 -0700

    bump up apiClient timeout to 15 seconds

--- packages/upswyng-web/src/utils/apiClient.ts
+++ packages/upswyng-web/src/utils/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: "https://upswyng-server.herokuapp.com/api/",
-  timeout: 10000,
+  timeout: 15000,
 });
 
 export default apiClient;
