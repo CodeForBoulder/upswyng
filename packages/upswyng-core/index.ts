948a9336cbcf9ac439a970d5124a5a346366c1e1
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2019-12-06 17:47:12 -0700

    Update web schedule view

---
+++ packages/upswyng-core/index.ts
@@ -0,0 +1,8 @@
+import ResourceSchedule_ from "./src/ResourceSchedule";
+import Time_ from "./src/Time";
+import { TScheduleItem as TScheduleItem_ } from "./src/ResourceSchedule";
+
+export const ResourceSchedule = ResourceSchedule_;
+export const Time = Time_;
+
+export type TScheduleItem = TScheduleItem_;
