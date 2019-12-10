a3f01f8cfa756b850e32c2d48218fd2ef904f33a
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2019-12-09 18:21:49 -0700

    Add clean script

---
+++ clean.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const glob = require("glob");
+
+const foldersToDelete = [
+  "./packages/upswyng-types/dist",
+  "./packages/upswyng-core/dist",
+  "./packages/upswyng-server/__build__",
+  "./packages/upswyng-web/build",
+];
+
+const globsToDelete = ["./packages/**/tsconfig*.tsbuildinfo"];
+
+function deleteFolderRecursive(path) {
+  if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
+    fs.readdirSync(path).forEach(function(file) {
+      const curPath = path + "/" + file;
+
+      if (fs.lstatSync(curPath).isDirectory()) {
+        // recurse
+        deleteFolderRecursive(curPath);
+      } else {
+        // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+
+    console.log(`  🗑  Deleting directory "${path}"...`);
+    fs.rmdirSync(path);
+  }
+}
+
+function deleteGlob(globString) {
+  glob.sync(globString, { cwd: ".", nodir: true }).forEach(f => {
+    console.log(`  🗑  Deleting file "${f}"...`);
+    fs.lstatSync(f).isFile() && fs.unlinkSync(f);
+  });
+}
+
+console.log("🧹  Cleaning build artifacts...");
+
+foldersToDelete.forEach(deleteFolderRecursive);
+globsToDelete.forEach(deleteGlob);
+
+console.log("🧼  Successfully cleaned build artifacts.");
