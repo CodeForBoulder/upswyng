33dc5c58f9102f0f99737bacb28924753d843e0d
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2019-12-11 02:51:32 -0700

    ✨ Create resource issue page (#187)
    * Create resource issue page
    
    * Remove conditional highlighting from the Issues nav button
    
    Didn’t work anyway

---
+++ packages/upswyng-server/src/routes/api/resources/issues/index.ts
@@ -0,0 +1,84 @@
+import { requireAdmin } from "../../../../utility/authHelpers";
+import ResourceIssue, {
+  resourceIssueDocumentToResourceIssue,
+} from "../../../../models/ResourceIssue";
+
+/**
+ * API endpoint to get a list of Resource Issues. Request body should have the following options:
+ * {
+ *   includeResolved: boolean, // default false,
+ *   limit: number, // default 20
+ *   minimumSeverity: "low" | "medium" | "high", // default low
+ *   offset: number, // default 0
+ * }
+ *
+ * The response has the shape of:
+ * {
+ *   count: number, // The total number of Resource Issues which match the query (ignoring limit)
+ *   estimatedTotal: number, // An estimate of the total number of Resource Issues (ignores entire query)
+ *   resourceIssues: TResourceIssue[], // Resource Issues which match the query
+ * }
+ */
+export async function post(req, res, next) {
+  try {
+    requireAdmin(req);
+  } catch {
+    res.writeHead(401, {
+      "Content-Type": "application/json",
+    });
+    return res.end(
+      JSON.stringify({
+        message: `You are not authorized to view the list of Resource Issues`,
+      })
+    );
+  }
+
+  const includeResolved: boolean = !!req.body.includeResolved;
+  const limit = req.body.limit || 20;
+  const offset = req.body.offset || 0;
+  const minimumSeverity = req.body.minimumSeverity || "low";
+
+  const includedSeverities = {
+    low: ["low", "medium", "high"],
+    medium: ["medium", "high"],
+    high: ["high"],
+  };
+
+  try {
+    const issues = await ResourceIssue.find({
+      resolved: { $in: [false, includeResolved] },
+      severity: { $in: includedSeverities[minimumSeverity] },
+    })
+      .skip(offset)
+      .limit(limit)
+      .sort({ createdAt: -1 });
+
+    const count = await ResourceIssue.find({
+      resolved: { $in: [false, includeResolved] },
+      severity: { $in: includedSeverities[minimumSeverity] },
+    }).countDocuments();
+
+    const estimatedTotal = await ResourceIssue.find().estimatedDocumentCount();
+
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+
+    return res.end(
+      JSON.stringify({
+        count,
+        estimatedTotal,
+        resourceIssues: issues.map(resourceIssueDocumentToResourceIssue),
+      })
+    );
+  } catch (e) {
+    res.writeHead(500, {
+      "Content-Type": "application/json",
+    });
+    return res.end(
+      JSON.stringify({
+        message: `Error getting Resource Issues: ${e.message}`,
+      })
+    );
+  }
+}
