77da468036a15f446522167f4e736a2b4fc4ff3d
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2019-12-20 01:02:42 -0700

    ♻️ Refactor server out for future testing (#189)

---
+++ packages/upswyng-server/src/app.ts
@@ -0,0 +1,50 @@
+/**
+ * The core of the API server. Does not include the sapper middleware, since
+ * that has to be imported in `server.js`. However, this is enough to test API
+ * endpoints.
+ */
+
+import { Connection } from "mongoose";
+import bodyParser from "body-parser";
+import compression from "compression";
+import connectMongo from "connect-mongo";
+import cors from "cors";
+import grant from "grant-express";
+import oidc from "grant-oidc";
+import polka from "polka";
+import session from "express-session";
+import sirv from "sirv";
+import userMiddleware from "./utility/userMiddleware";
+
+interface TAppOptions {
+  mongooseConnection: Connection;
+  dev: boolean; // start server in development mode
+  grantConfig: Record<string, any>; // see https://github.com/simov/grant#configuration
+  sessionSecret: string;
+}
+
+export default function(options: TAppOptions) {
+  const { dev, grantConfig, mongooseConnection, sessionSecret } = options;
+
+  const MongoStore = connectMongo(session);
+
+  return polka()
+    .use(
+      compression({ threshold: 0 }),
+      cors(), // TODO: Lock this down to non-admin routes
+      sirv("static", { dev }),
+      bodyParser.urlencoded({ extended: true }),
+      bodyParser.json(),
+      session({
+        store: new MongoStore({ mongooseConnection }),
+        secret: sessionSecret,
+        saveUninitialized: true,
+        resave: true,
+      }),
+      grant(grantConfig),
+      userMiddleware
+    )
+    .get("/callback", oidc(grantConfig), (_req, res) => {
+      res.redirect("/");
+    });
+}
