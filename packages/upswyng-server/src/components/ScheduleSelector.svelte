e28c3f43150ec1a75cea689afa3e2a71658de4bb
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2019-12-21 02:03:25 -0700

    :sparkles: Add ability to schedule selector to specify occurances of the day in a month

--- packages/upswyng-server/src/components/ScheduleSelector.svelte
+++ packages/upswyng-server/src/components/ScheduleSelector.svelte
@@ -10,6 +10,9 @@
 
   export let value; // TResourceSchedule
 
+  let isExpanded = false;
+  let recurrenceText = "";
+
   // Run once mounted so this logic is executed on the user's machine
   // instead of on our server.
   onMount(() => {
@@ -22,7 +25,7 @@
     }
   });
 
-  function addWeeklyRecurrence() {
+  function createRecurrenceRule(weeklyRepeatState, certainWeekRepeatState) {
     const dayToCode = {
       sunday: "SU",
       monday: "MO",
@@ -33,12 +36,38 @@
       saturday: "SA",
     };
 
-    if (!weeklyRepeatState.fromTime || !weeklyRepeatState.toTime) {
-      throw new Error("Please enter a 'from' and 'to' time");
-    }
     if (Object.keys(dayToCode).every(d => !weeklyRepeatState[d])) {
       throw new Error("Please select at least one day");
     }
+
+    const options = {
+      freq: certainWeekRepeatState.everyWeek ? RRule.WEEKLY : RRule.MONTHLY,
+
+      byweekday: Object.keys(dayToCode)
+        .map(d => (weeklyRepeatState[d] ? RRule[dayToCode[d]] : null))
+        .filter(Boolean)
+        .map(dayEnum => {
+          if (certainWeekRepeatState.everyWeek) {
+            return dayEnum;
+          } else {
+            return Object.entries(certainWeekRepeatState)
+              .filter(([k, v]) => v && k !== "everyWeek")
+              .map(([k, _v]) => dayEnum["nth"](k));
+          }
+        })
+        .flat(),
+    };
+    if (certainWeekRepeatState.everyWeek) {
+      options.interval = 1;
+    }
+    return new RRule(options);
+  }
+
+  function addWeeklyRecurrence() {
+    if (!weeklyRepeatState.fromTime || !weeklyRepeatState.toTime) {
+      throw new Error("Please enter a 'from' and 'to' time");
+    }
+
     const fromIndex = Time.options
       .map(o => o.label)
       .indexOf(weeklyRepeatState.fromTime.label);
@@ -57,13 +86,10 @@
       );
     }
 
-    const rrule = new RRule({
-      freq: RRule.WEEKLY,
-      interval: 1,
-      byweekday: Object.keys(dayToCode)
-        .map(d => (weeklyRepeatState[d] ? RRule[dayToCode[d]] : null))
-        .filter(Boolean),
-    });
+    const rrule = createRecurrenceRule(
+      weeklyRepeatState,
+      certainWeekRepeatState
+    );
 
     value.addItem({
       recurrenceRule: rrule,
@@ -71,8 +97,17 @@
       fromTime: weeklyRepeatState.fromTime,
       toTime: weeklyRepeatState.toTime,
     });
+
     weeklyRepeatState = Object.assign({}, weeklyRepeatStateDefault);
+    certainWeekRepeatState = Object.assign({}, certainWeekRepeatStateDefault);
     value = value; // make svelte update itself
+  }
+
+  function toggleExpanded() {
+    isExpanded = !isExpanded;
+    if (!isExpanded) {
+      certainWeekRepeatState = Object.assign({}, certainWeekRepeatStateDefault);
+    }
   }
 
   const weeklyRepeatStateDefault = {
@@ -90,8 +125,49 @@
     toTime: null,
   };
 
+  const certainWeekRepeatStateDefault = {
+    everyWeek: true,
+    1: true, // first <sunday, monday, ... saturday> of month
+    2: true, // second of month
+    3: true,
+    4: true,
+    [-2]: true, // second to last of month
+    [-1]: true, // last of month,
+  };
+
   let weeklyRepeatState = Object.assign({}, weeklyRepeatStateDefault);
+  let certainWeekRepeatState = Object.assign({}, certainWeekRepeatStateDefault);
+
+  $: {
+    try {
+      recurrenceText = createRecurrenceRule(
+        weeklyRepeatState,
+        certainWeekRepeatState
+      ).toText();
+    } catch (_e) {
+      recurrenceText = "";
+    }
+  }
 </script>
+
+<style>
+  .more-less-selector {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  .more-less-selector a {
+    cursor: pointer;
+    text-decoration: none;
+  }
+
+  .more-less-selector a span.icon {
+    position: relative;
+    top: 0.1em;
+    left: 0.15em;
+  }
+</style>
 
 <h1>Schedule</h1>
 
@@ -132,7 +208,7 @@
       }} />
   {/each}
 </div>
-<h2>Add Schedule Entry</h2>
+<h2 class="subtitle">Add Schedule Entry</h2>
 <div class="content">
   <fieldset>
     <div class="field has-addons">
@@ -174,7 +250,7 @@
         </span>
       </button>
     </div>
-    <div class="columns">
+    <div class="columns content">
       <div class="column">
         <label class="label" for="from-time">From</label>
         <Select
@@ -188,6 +264,81 @@
           items={Time.options} />
       </div>
     </div>
+    <div class="content">
+      {#if isExpanded}
+        <header class="more-less-selector">
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <a
+            on:click|preventDefault={() => toggleExpanded()}
+            class="has-text-grey-light is-uppercase is-size-6
+            has-text-weight-bold">
+            <span>Fewer Options</span>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <span class="icon is-small">
+              <i class="fas fa-chevron-up" aria-hidden="true" />
+            </span>
+          </a>
+        </header>
+      {:else}
+        <header class="more-less-selector">
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <a
+            on:click|preventDefault={() => toggleExpanded()}
+            class="has-text-grey-light is-uppercase is-size-6
+            has-text-weight-bold">
+            <span>More Options</span>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <span class="icon is-small">
+              <i class="fas fa-chevron-down" aria-hidden="true" />
+            </span>
+          </a>
+        </header>
+      {/if}
+    </div>
+    {#if isExpanded}
+      <div class="content">
+        <div class="field">
+          <label class="label">Certain occurances of the day in a month</label>
+        </div>
+        <div class="field has-addons">
+          <p class="control">
+            <button
+              aria-label="All occurances of the day"
+              class="button"
+              class:is-primary={certainWeekRepeatState.everyWeek}
+              on:click|preventDefault={() => {
+                certainWeekRepeatState = { ...certainWeekRepeatStateDefault };
+              }}
+              title="Select all occurances of the day">
+              <span>All</span>
+            </button>
+          </p>
+          {#each [{ name: 'First', value: 1 }, { name: 'Second', value: 2 }, { name: 'Third', value: 3 }, { name: 'Fourth', value: 4 }, { name: 'Last', value: -1 }, { name: 'Second-to-last', value: -2 }] as entry}
+            <p class="control">
+              <button
+                aria-label={entry.name}
+                class="button"
+                class:is-primary={certainWeekRepeatState[entry.value]}
+                on:click|preventDefault={() => {
+                  certainWeekRepeatState = { ...certainWeekRepeatState, everyWeek: false, [entry.value]: !certainWeekRepeatState[entry.value] };
+                }}>
+                <span>{entry.name}</span>
+              </button>
+            </p>
+          {/each}
+        </div>
+        {#if recurrenceText.length}
+          <div class="box">
+            {recurrenceText.charAt(0).toUpperCase() + recurrenceText.slice(1)}
+            {#if weeklyRepeatState.fromTime && weeklyRepeatState.toTime}
+              from {weeklyRepeatState.fromTime.label} to {weeklyRepeatState.toTime.label}
+            {:else}
+              <span class="has-text-grey is-italic">&lt;select times&gt;</span>
+            {/if}
+          </div>
+        {/if}
+      </div>
+    {/if}
     <div class="content">
       {#if weeklyRepeatState.fromTime && weeklyRepeatState.toTime && parseInt(weeklyRepeatState.fromTime.value, 10) >= parseInt(weeklyRepeatState.toTime.value, 10)}
         <p class="notification is-warning">
