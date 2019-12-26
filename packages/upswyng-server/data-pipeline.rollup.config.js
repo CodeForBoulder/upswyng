6edad7b173f60eb7314bf7e56f204e5b08bf198c
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2019-12-25 22:45:59 -0700

    ⬆️ Migrate deprecated rollup plugins (#196)
    Looked at migrating `rollup-plugin-typescript2` to `@rollup/plugin-typescript` but the API isn't exactly the same and `rollup-plugin-typescript2` hasn't been deprecated.

--- packages/upswyng-server/data-pipeline.rollup.config.js
+++ packages/upswyng-server/data-pipeline.rollup.config.js
@@ -1,8 +1,8 @@
 import commonjs from "@rollup/plugin-commonjs";
-import json from "rollup-plugin-json";
+import json from "@rollup/plugin-json";
 import pkg from "./package.json";
-import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
 import { createEnv, readConfigFile } from "@pyoner/svelte-ts-preprocess";
 
 const env = createEnv();
