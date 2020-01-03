ba77f671d83a4b847c6c0e05df11cb6b9175955f
rhinodavid <rhinodavid@github.com>
2020-01-02 20:32:19 -0700

    :white_check_mark: Add test for Category model with in-memory db

--- packages/upswyng-server/jest.config.js
+++ packages/upswyng-server/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
+  testEnvironment: "node",
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts$",
   moduleFileExtensions: ["ts", "js"],
   modulePathIgnorePatterns: ["node_modules"],
