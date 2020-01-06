7e0816806ebcecaec8397b7b19478193461b7f65
jacobvenable <jacob.venable@gmail.com>
2020-01-05 21:29:17 -0700

    create initial MUI theme files

---
+++ packages/upswyng-web/src/theme/palette.ts
@@ -0,0 +1,41 @@
+const palette = {
+  common: { black: "#000", white: "#fff" },
+  background: { paper: "#262626", default: "#3a3a3a" },
+  primary: {
+    main: "#F05A28",
+    contrastText: "#000",
+  },
+  secondary: {
+    main: "#F4BD21",
+    contrastText: "#000",
+  },
+  error: {
+    main: "#BC2222",
+    contrastText: "#fff",
+  },
+  text: {
+    primary: "#fff",
+    secondary: "#CECECE",
+    disabled: "#fff",
+    hint: "#919191",
+  },
+  blue: "#2C4198",
+  brown: "#AA6A03",
+  charcoal: "#3a3a3a",
+  gold: "#F4BD21",
+  green: "#68A840",
+  greyDark: "#3c3c3c",
+  greyMedium: "#707070",
+  greyLight: "#A2A2A2",
+  lavendar: "#9F6CBA",
+  orangePrimary: "#F4855F",
+  orangeDark: "#CE5A30",
+  orangeSecondary: "#DB5427",
+  pink: "#B146BF",
+  purple: "#5B4DC7",
+  red: "#BC2222",
+  rosewood: "#795161",
+  teal: "#40ACBF",
+};
+
+export default palette;
