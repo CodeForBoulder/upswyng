041701adc94e44632ff7869af0fc07ed37937c4d
jacobvenable <jacob.venable@gmail.com>
2020-01-05 22:54:52 -0700

    update <SearchInput/> to functional component

--- packages/upswyng-web/src/components/SearchInput.tsx
+++ packages/upswyng-web/src/components/SearchInput.tsx
@@ -1,80 +1,70 @@
-import { InputAdornment, TextField } from "@material-ui/core";
-import React, { Component } from "react";
+import { Input, InputAdornment } from "@material-ui/core";
+import FormControl from "@material-ui/core/FormControl";
+import React from "react";
 import { Redirect } from "react-router";
 import { SEARCH_PARAM_QUERY } from "../constants";
 import SearchIcon from "@material-ui/icons/Search";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const useInputStyles = makeStyles(theme => ({
-  input: {
+  root: {
     background: theme.palette.common.white,
+    color: theme.palette.common.black,
+    "&:hover": {
+      background: theme.palette.common.white,
+    },
   },
 }));
 
-interface State {
-  query: string;
-  submitted: boolean;
-}
+const Search = () => {
+  const inputClasses = useInputStyles();
+  const [query, setQuery] = React.useState("");
+  const [submitted, setSubmitted] = React.useState(false);
 
-class Search extends Component {
-  inputClasses = useInputStyles();
-
-  state: State = {
-    query: "",
-    submitted: false,
-  };
-
-  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {
       target: { value },
     } = e;
 
-    this.setState({
-      query: value,
-    });
+    setQuery(value);
   };
 
-  handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    this.setState({
-      submitted: true,
-    });
+    setSubmitted(true);
   };
 
-  render() {
-    const { submitted, query } = this.state;
-    return (
-      <>
-        <form onSubmit={this.handleSubmit} className="search">
-          <TextField
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="search">
+        <FormControl fullWidth hiddenLabel>
+          <Input
             aria-label="search"
             fullWidth
             placeholder="Search - What are your needs today?"
             id="search"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }}
+            classes={inputClasses}
+            startAdornment={
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            }
             name="search"
-            onChange={this.handleChange}
+            onChange={handleChange}
             value={query}
-            variant="filled"
           />
-        </form>
-        {submitted ? (
-          <Redirect
-            to={{
-              pathname: "/search",
-              search: `?${SEARCH_PARAM_QUERY}=${query}`,
-            }}
-          />
-        ) : null}
-      </>
-    );
-  }
-}
+        </FormControl>
+      </form>
+      {submitted ? (
+        <Redirect
+          to={{
+            pathname: "/search",
+            search: `?${SEARCH_PARAM_QUERY}=${query}`,
+          }}
+        />
+      ) : null}
+    </>
+  );
+};
 
 export default Search;
