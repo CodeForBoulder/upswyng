f72c1d7326f40b9672c706fa4eedddb75a491171
jacobvenable <jacob.venable@gmail.com>
2020-01-05 21:30:25 -0700

    remove unused colors from palette

--- packages/upswyng-web/src/theme/palette.ts
+++ packages/upswyng-web/src/theme/palette.ts
@@ -19,23 +19,6 @@ const palette = {
     disabled: "#fff",
     hint: "#919191",
   },
-  blue: "#2C4198",
-  brown: "#AA6A03",
-  charcoal: "#3a3a3a",
-  gold: "#F4BD21",
-  green: "#68A840",
-  greyDark: "#3c3c3c",
-  greyMedium: "#707070",
-  greyLight: "#A2A2A2",
-  lavendar: "#9F6CBA",
-  orangePrimary: "#F4855F",
-  orangeDark: "#CE5A30",
-  orangeSecondary: "#DB5427",
-  pink: "#B146BF",
-  purple: "#5B4DC7",
-  red: "#BC2222",
-  rosewood: "#795161",
-  teal: "#40ACBF",
 };
 
 export default palette;
