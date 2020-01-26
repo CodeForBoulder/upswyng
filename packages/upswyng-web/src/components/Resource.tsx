69b35fbcd15b1e3615430a57ae7a6cd74f62ae68
jacobvenable <jacob.venable@gmail.com>
2020-01-25 17:42:43 -0700

    feat: update <Resource/> to use typography and grid from MUI

--- packages/upswyng-web/src/components/Resource.tsx
+++ packages/upswyng-web/src/components/Resource.tsx
@@ -1,8 +1,8 @@
 import { Container, colors } from "../App.styles";
-import Details, { DetailBody, DetailHeading } from "./Details";
 import { TCategoryDefinition, categories } from "./Categories";
 
 import BannerColorContext from "./BannerColorContext";
+import Grid from "@material-ui/core/Grid";
 import LoadingSpinner from "./LoadingSpinner";
 import Map from "./Map";
 import PageBanner from "./PageBanner";
@@ -11,6 +11,7 @@ import { SEARCH_PARAM_RESOURCE } from "../constants";
 import Schedule from "./Schedule";
 import Services from "./Services";
 import { TResource } from "@upswyng/upswyng-types";
+import Typography from "@material-ui/core/Typography";
 import { getSearchParamVal } from "../utils/searchParams";
 import useResource from "./useResource";
 
@@ -18,63 +19,6 @@ interface Props {
   id: string;
   resource: TResource;
 }
-
-const renderAddressContent = (resource: TResource) => {
-  const {
-    address: { address1, address2, city, state, zip },
-  } = resource;
-
-  if (!address1 || !city || !state || !zip) {
-    return <></>;
-  }
-
-  return (
-    <>
-      <DetailHeading>Address</DetailHeading>
-      <DetailBody>
-        <p>
-          {" "}
-          {address1}, {address2 ? `${address2},` : ""} {city}, {state} {zip}
-        </p>
-      </DetailBody>
-    </>
-  );
-};
-
-const renderPhoneContent = (resource: TResource) => {
-  const { phone } = resource;
-  if (!phone) {
-    return <></>;
-  }
-  return (
-    <>
-      <DetailHeading>Phone</DetailHeading>
-      <DetailBody>
-        <p>{phone}</p>
-      </DetailBody>
-    </>
-  );
-};
-
-const renderWebsiteContent = (resource: TResource) => {
-  const { website } = resource;
-
-  if (!website) {
-    return <></>;
-  }
-  return (
-    <>
-      <DetailHeading>Website</DetailHeading>
-      <DetailBody>
-        <p>
-          <a href={website} target="_blank" rel="noopener noreferrer">
-            {website}
-          </a>
-        </p>
-      </DetailBody>
-    </>
-  );
-};
 
 const renderErrorMessage = () => (
   <p>We&apos;re sorry, this service was not found.</p>
@@ -129,33 +73,64 @@ export const Resource = () => {
     : "black";
 
   return (
-    <Container>
-      <PageBanner
-        color={currentBannerColor || colors[resourceCategoryColorName]}
-        text={name}
-      />
-      <Details>
-        {renderAddressContent(resource)}
-        {renderPhoneContent(resource)}
-        {renderWebsiteContent(resource)}
-        <DetailHeading>Schedule</DetailHeading>
-        <DetailBody>
-          <Schedule schedule={schedule} />
-        </DetailBody>
-        <DetailHeading>Services</DetailHeading>
-        <DetailBody>
-          <Services resource={resource} />
-        </DetailBody>
-      </Details>
-      {resource.latitude && resource.longitude && (
-        <Map
-          address={resource.address}
-          name={resource.name}
-          latitude={resource.latitude}
-          longitude={resource.longitude}
+    <>
+      <Container>
+        <PageBanner
+          color={currentBannerColor || colors[resourceCategoryColorName]}
+          text={name}
         />
-      )}
-    </Container>
+      </Container>
+      <Container>
+        <Grid container direction="column" spacing={2}>
+          {resource.address && (
+            <Grid item>
+              <Typography variant="h2">Address</Typography>
+              <Typography>
+                {resource.address.address1}, {resource.address.address2}{" "}
+                {resource.address.city}, {resource.address.state}{" "}
+                {resource.address.zip}
+              </Typography>
+            </Grid>
+          )}
+          {resource.phone && (
+            <Grid item>
+              <Typography variant="h2">Phone</Typography>
+              <Typography>{resource.phone}</Typography>
+            </Grid>
+          )}
+          {resource.website && (
+            <Grid item>
+              <Typography variant="h2">Website</Typography>
+              <Typography>
+                <a
+                  href={resource.website}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {resource.website}
+                </a>
+              </Typography>
+            </Grid>
+          )}
+          <Grid item>
+            <Typography variant="h2">Schedule</Typography>
+            <Schedule schedule={schedule} />
+          </Grid>
+          <Grid item>
+            <Typography variant="h2">Services</Typography>
+            <Services resource={resource} />
+          </Grid>
+        </Grid>
+        {resource.latitude && resource.longitude && (
+          <Map
+            address={resource.address}
+            name={resource.name}
+            latitude={resource.latitude}
+            longitude={resource.longitude}
+          />
+        )}
+      </Container>
+    </>
   );
 };
 
