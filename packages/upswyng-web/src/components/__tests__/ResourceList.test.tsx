4d934ba368db8c3aa1ed3acc93faafbb26fb9cb4
jacobvenable <jacob.venable@gmail.com>
2020-02-19 01:05:07 -0700

    chore: create initial ResourceList test

---
+++ packages/upswyng-web/src/components/__tests__/ResourceList.test.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ResourceList from "../ResourceList";
+import { mockResources } from "./data-mocks";
+import { mount } from "enzyme";
+
+jest.mock("../LoadingSpinner", () => () => "TEST-loading-spinner");
+jest.mock("../ResourceCard", () => () => (
+  <div data-test="TEST-resource-card" />
+));
+
+describe("<ResourceList/>", () => {
+  function renderWith(overrides = {}) {
+    return mount(<ResourceList resources={mockResources} {...overrides} />);
+  }
+
+  it("renders a loading spinner when resources are not loaded", () => {
+    const resources = undefined;
+    const subject = renderWith({ resources });
+
+    expect(subject.text()).toContain("TEST-loading-spinner");
+  });
+
+  it("renders a resource for each resource in the list", () => {
+    const resources = mockResources;
+    const subject = renderWith({ resources });
+
+    expect(subject.find('[data-test="TEST-resource-card"]')).toHaveLength(
+      resources.length
+    );
+  });
+});
