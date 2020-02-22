64792e31f3fd93043ae5816d5ca0ba3cf55a58e2
jacobvenable <jacob.venable@gmail.com>
2020-02-22 13:00:14 -0700

    feat: update <SearchResults/> to have search input

--- packages/upswyng-web/src/components/SearchResults.tsx
+++ packages/upswyng-web/src/components/SearchResults.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ResourceList from "./ResourceList";
 import { Response } from "algoliasearch";
+import { SEARCH_PARAM_QUERY } from "../constants";
+import SearchInput from "./SearchInput";
+import debounce from "debounce";
+import { useHistory } from "react-router-dom";
+import useSearchParam from "./useSearchParam";
+import useSearchResults from "./useSearchResults";
 
 interface Props {
   placeholder?: React.ReactElement;
-  results?: Response | null;
 }
 
 const mapResultsToResources = (
@@ -25,11 +30,34 @@ const mapResultsToResources = (
   }));
 };
 
-const SearchResults = ({ placeholder, results }: Props) => (
-  <ResourceList
-    placeholder={placeholder}
-    resources={mapResultsToResources(results)}
-  />
-);
+const SearchResults = ({ placeholder }: Props) => {
+  const searchQuery = useSearchParam(SEARCH_PARAM_QUERY);
+  const results = useSearchResults(searchQuery || "");
+  const history = useHistory();
+
+  const updateSearch = debounce((searchQuery: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(SEARCH_PARAM_QUERY, searchQuery);
+
+    history.replace({
+      search: searchParams.toString(),
+    });
+  }, 300);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchQuery = e.target.value;
+    updateSearch(searchQuery);
+  };
+
+  return (
+    <>
+      <SearchInput onChange={handleChange} value={searchQuery} />
+      <ResourceList
+        placeholder={placeholder}
+        resources={mapResultsToResources(results)}
+      />
+    </>
+  );
+};
 
 export default SearchResults;
