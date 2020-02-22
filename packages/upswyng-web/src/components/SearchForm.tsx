960687f26d56d87139fe05c2643add09c7c78c62
jacobvenable <jacob.venable@gmail.com>
2020-02-22 14:33:02 -0700

    feat: replace use of redirect with history.psuh in <SearchForm/>

--- packages/upswyng-web/src/components/SearchForm.tsx
+++ packages/upswyng-web/src/components/SearchForm.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Redirect } from "react-router";
 import { SEARCH_PARAM_QUERY } from "../constants";
 import SearchInput from "./SearchInput";
+import { useHistory } from "react-router-dom";
 
 const SearchForm = () => {
+  const history = useHistory();
   const [query, setQuery] = React.useState<string>("");
-  const [submitted, setSubmitted] = React.useState<boolean>(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
@@ -13,23 +13,18 @@ const SearchForm = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    setSubmitted(true);
+    if (query) {
+      history.push({
+        pathname: "/search",
+        search: `?${SEARCH_PARAM_QUERY}=${query}`,
+      });
+    }
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit} className="search">
-        <SearchInput onChange={handleChange} value={query} />
-      </form>
-      {submitted && (
-        <Redirect
-          to={{
-            pathname: "/search",
-            search: `?${SEARCH_PARAM_QUERY}=${query}`,
-          }}
-        />
-      )}
-    </>
+    <form onSubmit={handleSubmit} className="search">
+      <SearchInput onChange={handleChange} value={query} />
+    </form>
   );
 };
 
