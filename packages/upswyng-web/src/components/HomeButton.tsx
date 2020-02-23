f94c3fa479ddf672583395e869e35c4d8e1071ee
jacobvenable <jacob.venable@gmail.com>
2020-02-23 11:05:33 -0700

    feat: replace use of styled-components with MUI styles in  <HomeButton/>

--- packages/upswyng-web/src/components/HomeButton.tsx
+++ packages/upswyng-web/src/components/HomeButton.tsx
@@ -2,47 +2,46 @@ import { colors, font } from "../App.styles";
 
 import { ButtonProps } from "@material-ui/core/Button";
 import React from "react";
-import styled from "styled-components";
-
-interface HomeLinkPropsBase {
-  children: React.ReactElement | React.ReactElement[];
-  key: string;
-}
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 interface HomeButtonProps extends ButtonProps {
-  readonly buttonColor: string;
+  buttonColor: string;
+  children: React.ReactNode | React.ReactNode[];
 }
 
-const HomeButton = styled.span`
-  && {
-    align-items: stretch;
-    background: ${(props: HomeButtonProps) =>
-      props.buttonColor || colors.greyDark};
-    border-radius: 0;
-    color: ${colors.white};
-    display: flex;
-    flex-direction: column;
-    font-family: ${font.families.openSans};
-    font-size: ${font.helpers.convertPixelsToRems(22)};
-    font-weight: 700;
-    justify-content: space-between;
-    line-height: ${font.helpers.convertPixelsToRems(24)};
-    padding: ${font.helpers.convertPixelsToRems(10)};
-    text-decoration: none;
-    text-transform: none;
-    width: 100%;
-  }
-  &&:hover {
-    background: ${(props: HomeButtonProps) =>
-      props.buttonColor || colors.greyDark};
-    filter: brightness(95%);
-  }
-  svg {
-    align-self: flex-end;
-    height: auto;
-    max-height: ${font.helpers.convertPixelsToRems(45)};
-    width: ${font.helpers.convertPixelsToRems(42)};
-  }
-`;
+const useStyles = makeStyles({
+  button: (props: HomeButtonProps) => ({
+    alignItems: "stretch",
+    background: props.buttonColor || colors.greyDark,
+    borderRadius: 0,
+    color: colors.white,
+    display: "flex",
+    flexDirection: "column",
+    fontFamily: font.families.openSans,
+    fontSize: font.helpers.convertPixelsToRems(22),
+    fontWeight: 700,
+    justifyContent: "space-between",
+    lineHeight: font.helpers.convertPixelsToRems(24),
+    padding: font.helpers.convertPixelsToRems(10),
+    textDecoration: "none",
+    textTransform: "none",
+    width: "100%",
+    "&:hover": {
+      background: props.buttonColor || colors.greyDark,
+      filter: "brightness(95%)",
+    },
+    "& svg": {
+      alignSelf: "flex-end",
+      height: "auto",
+      maxHeight: font.helpers.convertPixelsToRems(45),
+      width: font.helpers.convertPixelsToRems(42),
+    },
+  }),
+});
+
+const HomeButton = (props: HomeButtonProps) => {
+  const buttonClasses = useStyles(props);
+  return <span className={buttonClasses.button}>{props.children}</span>;
+};
 
 export default HomeButton;
