9bd5b868d7683d8a8b699c96ff7d58bd303aafc2
jollyjerr <jollyjerr@gmail.com>
2020-03-11 20:29:21 -0600

    :recycle: updt: refactor cache return

--- packages/upswyng-server/src/routes/api/weather.ts
+++ packages/upswyng-server/src/routes/api/weather.ts
@@ -2,7 +2,7 @@ import Cache from "../../utility/cache";
 import { TWeatherCurrentResponse } from "@upswyng/upswyng-types";
 import axios from "axios";
 
-const cache = new Cache();
+const cache = new Cache<TWeatherCurrentResponse>();
 
 const getCurrentWeather = async (
   latitude: number | null,
@@ -28,10 +28,10 @@ const getCurrentWeather = async (
 const getCachedWeatherData = async (
   latitude: number,
   longitude: number
-): Promise<Record<string, any>> => {
+): Promise<TWeatherCurrentResponse> => {
   const key = `${latitude}${longitude}`;
   if (!cache.getValue(key) || cache.isExpired(key, new Date())) {
-    cache.setValue(key, getCurrentWeather(latitude, longitude));
+    cache.setValue(key, await getCurrentWeather(latitude, longitude));
     cache.setExpiration(key, new Date(), 60000);
   }
   return cache.getValue(key);
@@ -43,11 +43,10 @@ export async function get(req, res) {
       +req.query.latitude,
       +req.query.longitude
     );
-    res.writeHead(200, { "Content-Type": "application/json" });
-    return res.end(JSON.stringify({ ...result }));
+    return res.status(200).json({ ...result });
   } catch (e) {
     console.error(e);
-    return res.setStatus(500).json({
+    return res.status(500).json({
       message: `Error fetching weather information: ${e.message}`,
     });
   }
