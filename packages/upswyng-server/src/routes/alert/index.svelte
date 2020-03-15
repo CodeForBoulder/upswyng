55bb78c479d52cdb8e2abfcaa745d91797f176ac
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2020-03-15 17:13:42 -0600

    ✨ [server] Create submission endpoint for alerts (#297)

---
+++ packages/upswyng-server/src/routes/alert/index.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { stores } from "@sapper/app";
+  import { readFlashMessages } from "../../utility/flashMessage.ts";
+
+  const { session } = stores();
+  const flashMessages = readFlashMessages(session);
+</script>
+
+<svelte:head>
+  <title>Upswyng: Alerts</title>
+</svelte:head>
+
+<section class="section">
+  {#each flashMessages as flashMessage}
+    <div
+      class="notification"
+      class:is-success={flashMessage.type === 'success'}
+      class:is-danger={flashMessage.type === 'error'}>
+      {flashMessage.message}
+    </div>
+  {/each}
+  <div class="container">
+    <h1 class="title">
+      Alerts
+      <span class="tag is-dark">Admin</span>
+    </h1>
+  </div>
+</section>
