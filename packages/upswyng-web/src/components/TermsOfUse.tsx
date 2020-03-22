bc3d38f638674acf97d92bad19aa3f1e12088dcc
jacobvenable <jacob.venable@gmail.com>
2020-03-22 00:50:06 -0600

    feat: implement MUI typography in TermsOfUse page

--- packages/upswyng-web/src/components/TermsOfUse.tsx
+++ packages/upswyng-web/src/components/TermsOfUse.tsx
@@ -1,12 +1,13 @@
 import Container from "@material-ui/core/Container";
 import PageBanner from "./PageBanner";
 import React from "react";
+import Typography from "@material-ui/core/Typography";
 import { colors } from "../App.styles";
 
 const TermsOfUse = () => (
   <Container>
     <PageBanner color={colors.orangeDark} text={"Terms of Use"} />
-    <p>
+    <Typography paragraph>
       Upswyng provides its content on this website (the “Site”) and mobile
       applications (the “App”) which enable users to locate resources including
       food banks, medical centers, and shelters (the “Services”) subject to the
@@ -15,10 +16,14 @@ const TermsOfUse = () => (
       such updated Terms at this location. Your continued use of the Services
       after such posting confirms your consent to be bound by the Terms, as
       amended.
-    </p>
-    <h2>1. The Services</h2>
-    <h3>License.</h3>
-    <p>
+    </Typography>
+    <Typography gutterBottom variant="h2">
+      1. The Services
+    </Typography>
+    <Typography gutterBottom variant="h3">
+      License.
+    </Typography>
+    <Typography paragraph>
       Subject to compliance with these Terms, Upswyng grants you a limited,
       non-exclusive, nonsublicensable, revocable, non-transferable license to:
       (i) access and use the Site, or the App on your personal device solely in
@@ -26,9 +31,11 @@ const TermsOfUse = () => (
       content, information, and related information that may be made available
       through the Services, in each case solely for your personal, noncommercial
       use. Any rights not expressely granted herein are reserved Upswyng.
-    </p>
-    <h3>Restrictions.</h3>
-    <p>
+    </Typography>
+    <Typography gutterBottom variant="h3">
+      Restrictions.
+    </Typography>
+    <Typography paragraph>
       You may not: (i) remove any copyright, trademark or other proprietary
       notices from any portion of the Services; (ii) reproduce, modify, prepare
       derivative works based upon, distribute, license, lease, sell, resell,
@@ -42,9 +49,11 @@ const TermsOfUse = () => (
       the operation and/or functionality of any aspect of the Services; or (vi)
       attempt to gain unauthorized access to or impair any aspect of the
       Services or its related systems or networks.
-    </p>
-    <h3>Ownership.</h3>
-    <p>
+    </Typography>
+    <Typography gutterBottom variant="h3">
+      Ownership.
+    </Typography>
+    <Typography paragraph>
       The Services and all rights therein are and shall remain Upswyng&apos;s
       property or the property of Upswyng&apos;s licensors. Neither these Terms
       nor your use of the Services convey or grant to you any rights: (i) in or
@@ -52,17 +61,23 @@ const TermsOfUse = () => (
       (ii) to use or reference in any manner Upswyng&apos;s company names,
       logos, product and service names, trademarks or services marks or those of
       Upswyng&apos;s licensors.
-    </p>
-    <h3>Copyrights.</h3>
-    <p>
+    </Typography>
+    <Typography gutterBottom variant="h3">
+      Copyrights.
+    </Typography>
+    <Typography paragraph>
       All content and functionality on the Site, including text, graphics,
       logos, icons, and images and the selection and arrangement thereof is the
       exclusive property of Upswyng or its licensors and is protected by U.S.
       copyright laws. All rights not expressly granted are reserved.
-    </p>
-    <h2>2. Disclaimers; Limitation of Liability; Indemnity</h2>
-    <h3>DISCLAIMER.</h3>
-    <p>
+    </Typography>
+    <Typography gutterBottom variant="h2">
+      2. Disclaimers; Limitation of Liability; Indemnity
+    </Typography>
+    <Typography gutterBottom variant="h3">
+      DISCLAIMER.
+    </Typography>
+    <Typography paragraph>
       THE SERVICES ARE PROVIDED &ldquo;AS IS&rdquo; AND &ldquo;AS
       AVAILABLE.&rdquo; UPSWYNG DISCLAIMS ALL REPRESENTATIONS AND WARRANTIES,
       EXPRESS, IMPLIED, OR STATUTORY, NOT EXPRESSLY SET OUT IN THESE TERMS,
@@ -76,9 +91,11 @@ const TermsOfUse = () => (
       AGREE THAT THE ENTIRE RISK ARISING OUT OF YOUR USE OF THE SERVICES, AND
       ANY SERVICE OR GOOD REQUESTED IN CONNECTION THEREWITH, REMAINS SOLELY WITH
       YOU, TO THE MAXIMUM EXTENT PERMITTED UNDER APPLICABLE LAW.
-    </p>
-    <h3>LIMITATION OF LIABILITY.</h3>
-    <p>
+    </Typography>
+    <Typography gutterBottom variant="h3">
+      LIMITATION OF LIABILITY.
+    </Typography>
+    <Typography paragraph>
       UPSWYNG SHALL NOT BE LIABLE FOR INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
       PUNITIVE, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, LOST DATA,
       PERSONAL INJURY, OR PROPERTY DAMAGE RELATED TO, IN CONNECTION WITH, OR
@@ -87,22 +104,22 @@ const TermsOfUse = () => (
       EVEN IF UPSWYNG HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES. USE
       OF THE UPSWYNG SITE, APP, OR ANY INFROMATION CONTAINED ON IT IS AT YOUR
       OWN RISK.
-    </p>
-    <p>
+    </Typography>
+    <Typography paragraph>
       UPSWYNG SHALL NOT BE LIABLE FOR ANY DAMAGES, LIABILITY OR LOSSES ARISING
       OUT OF YOUR USE OF OR RELIANCE ON THE SERVICES OR YOUR INABILITY TO ACCESS
       OR USE THE SERVICES. UPSWYNG SHALL NOT BE LIABLE FOR DELAY OR FAILURE IN
       PERFORMANCE RESULTING FROM CAUSES BEYOND UPSWYNG&apos;s REASONABLE
       CONTROL.
-    </p>
-    <p>
+    </Typography>
+    <Typography paragraph>
       THE SERVICES MAY BE USED BY YOU TO REQUEST AND LOCATE SERVICES OUTLINED
       ABOVE WITH THIRD PARTY PROVIDERS, BUT YOU AGREE THAT UPSWYNG HAS NO
       RESPONSIBILITY OR LIABILITY TO YOU RELATED TO ANY OF THE SERVICES PROVIDED
       TO YOU BY THIRD PARTY PROVIDERS OTHER THAN AS EXPRESSLY SET FORTH IN THESE
       TERMS.
-    </p>
-    <p>
+    </Typography>
+    <Typography paragraph>
       THE LIMITATIONS AND DISCLAIMER IN THIS SECTION DO NOT PURPORT TO LIMIT
       LIABILITY OR ALTER YOUR RIGHTS AS A CONSUMER THAT CANNOT BE EXCLUDED UNDER
       APPLICABLE LAW. BECAUSE SOME STATES ORJURISDICTIONS DO NOT ALLOW THE
@@ -110,31 +127,39 @@ const TermsOfUse = () => (
       INCIDENTAL DAMAGES, IN SUCH STATES OR JURISDICTIONS, UPSWYNG’S LIABILITY
       SHALL BE LIMITED TO THE EXTENT PERMITTED BY LAW. THIS PROVISION SHALL HAVE
       NO EFFECT ON UPSWYNG’S CHOICE OF LAW PROVISION SET FORTH BELOW.
-    </p>
-    <h3>Indemnity.</h3>
-    <p>
+    </Typography>
+    <Typography gutterBottom variant="h3">
+      Indemnity.
+    </Typography>
+    <Typography paragraph>
       You agree to indemnify and hold Upswyng and its affiliates and their
       officers, directors, employees, and agents harmless from any and all
       claims, demands, losses, liabilities, and expenses (including
       attorneys&apos; fees), arising out of or in connection with: (i) your use
       of the Services or services or goods obtained through your use of the
       Services; or (ii) your breach or violation of any of these Terms.
-    </p>
-    <h2>3. General</h2>
-    <h3>Privacy Policy</h3>
-    <p>
+    </Typography>
+    <Typography gutterBottom variant="h2">
+      3. General
+    </Typography>
+    <Typography gutterBottom variant="h3">
+      Privacy Policy
+    </Typography>
+    <Typography paragraph>
       In accordance with the terms of upswyng.org&apos;s Privacy Policy, Upswyng
       respect&apos;s the privacy of its Users. Upswyng recommends users read its
       Privacy Policy, available in the main menu, before using the website or
       app.
-    </p>
-    <h3>Governing Law; Jurisdiction.</h3>
-    <p>
+    </Typography>
+    <Typography gutterBottom variant="h3">
+      Governing Law; Jurisdiction.
+    </Typography>
+    <Typography paragraph>
       These Terms are governed by the laws of the State of Colorado without
       reference to the principles of conflicts of laws thereof. Any dispute
       arising from these Terms shall be resolved exclusively in the state and
       federal courts of Colorado.
-    </p>
+    </Typography>
   </Container>
 );
 
