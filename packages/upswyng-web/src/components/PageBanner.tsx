8db2473d0dab428ca01f7375ff51d97615cb0487
jacobvenable <jacob.venable@gmail.com>
2020-03-21 18:45:41 -0600

    feat: remove use of styled components from <PageBanner/> and <BackButton/>

--- packages/upswyng-web/src/components/PageBanner.tsx
+++ packages/upswyng-web/src/components/PageBanner.tsx
@@ -2,32 +2,15 @@ import { colors, font } from "../App.styles";
 
 import BackButton from "./BackButton";
 import BannerColorContext from "./BannerColorContext";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 import React from "react";
-import styled from "styled-components";
 
 interface Props {
   color?: string;
   text: string;
   backButtonAction?: (() => void) | null;
 }
-
-const PageBannerContainer = styled.div`
-  align-items: stretch;
-  background: ${props => (props.color ? props.color : colors.greyMedium)};
-  display: flex;
-  flex-direction: row;
-  margin: 0 0 ${font.helpers.convertPixelsToRems(12)};
-  padding: ${font.helpers.convertPixelsToRems(14)} 0;
-  wrap: no-wrap;
-`;
-
-const PageBannerHeading = styled.h1`
-  align-items: center;
-  display: flex;
-  font-size: ${font.helpers.convertPixelsToRems(24)};
-  font-weight: 400;
-  margin: ${font.helpers.convertPixelsToRems(-2)} 0 0;
-`;
 
 const PageBanner = ({ color, text, backButtonAction }: Props) => {
   const { currentBannerColor, updateCurrentBannerColor } = React.useContext(
@@ -39,10 +22,18 @@ const PageBanner = ({ color, text, backButtonAction }: Props) => {
   }, [color, updateCurrentBannerColor]);
 
   return (
-    <PageBannerContainer color={currentBannerColor || colors.black}>
-      <BackButton backButtonAction={backButtonAction} />
-      <PageBannerHeading>{text}</PageBannerHeading>
-    </PageBannerContainer>
+    <Box bgcolor={currentBannerColor || colors.black} py={2}>
+      <Grid container wrap="nowrap" alignItems="center" spacing={1}>
+        <Grid item>
+          <BackButton backButtonAction={backButtonAction} />
+        </Grid>
+        <Grid item>
+          <Box component="h1" fontSize={font.helpers.convertPixelsToRems(24)}>
+            {text}
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
