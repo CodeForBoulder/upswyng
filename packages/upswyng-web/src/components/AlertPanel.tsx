7318cf2915648a06c1f3882b51c214c786131664
jacobvenable <jacob.venable@gmail.com>
2020-03-26 21:11:55 -0600

    feat: separate alert panels from alert drawer

---
+++ packages/upswyng-web/src/components/AlertPanel.tsx
@@ -0,0 +1,55 @@
+import Avatar from "@material-ui/core/Avatar";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import Grid from "@material-ui/core/Grid";
+import React from "react";
+import { TAlertFull } from "@upswyng/upswyng-types";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+import Typography from "@material-ui/core/Typography";
+import makeStyles from "@material-ui/styles/makeStyles";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  avatar: (alert: TAlertFull) => ({
+    backgroundColor: alert.color || theme.palette.primary.main,
+    color: theme.palette.common.white,
+  }),
+  expansionPanel: (alert: TAlertFull) => ({
+    "&::before": {
+      backgroundColor: alert.color || theme.palette.primary.main,
+    },
+  }),
+}));
+
+interface Props {
+  alert: TAlertFull;
+}
+
+const AlertPanel = ({ alert }: Props) => {
+  const classes = useStyles(alert);
+  return (
+    <ExpansionPanel className={classes.expansionPanel}>
+      <ExpansionPanelSummary>
+        <Grid alignItems="center" container spacing={3} wrap="nowrap">
+          <Grid item>
+            <Avatar className={classes.avatar}>
+              <span className={alert.icon} />
+            </Avatar>
+          </Grid>
+          <Grid item>
+            <Typography component="h2" variant="h3">
+              {alert.title}
+            </Typography>
+          </Grid>
+        </Grid>
+      </ExpansionPanelSummary>
+      {alert.detail && (
+        <ExpansionPanelDetails>
+          <Typography>{alert.detail}</Typography>
+        </ExpansionPanelDetails>
+      )}
+    </ExpansionPanel>
+  );
+};
+
+export default AlertPanel;
--- packages/upswyng-web/src/components/AlertsDrawer.tsx
+++ packages/upswyng-web/src/components/AlertsDrawer.tsx
@@ -1,17 +1,6 @@
 import Drawer, { DrawerProps } from "@material-ui/core/Drawer";
-import Avatar from "@material-ui/core/Avatar";
-import Box from "@material-ui/core/Box";
+import AlertPanel from "./AlertPanel";
 import Container from "@material-ui/core/Container";
-import ExpandLess from "@material-ui/icons/ExpandLess";
-import ExpandMore from "@material-ui/icons/ExpandMore";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import Grid from "@material-ui/core/Grid";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import ListItemText from "@material-ui/core/ListItemText";
 import React from "react";
 import { TAlertFull } from "@upswyng/upswyng-types";
 import { Theme } from "@material-ui/core/styles/createMuiTheme";
@@ -35,6 +24,7 @@ const AlertsDrawer = ({ alerts, ...drawerProps }: Props) => {
     <Drawer
       anchor="top"
       {...drawerProps}
+      open
       PaperProps={{ className: classes.drawerPaper }}
     >
       <Container>
@@ -42,29 +32,7 @@ const AlertsDrawer = ({ alerts, ...drawerProps }: Props) => {
           Alerts
         </Typography>
         {alerts &&
-          alerts.map(alert => (
-            <ExpansionPanel key={alert.title}>
-              <ExpansionPanelSummary>
-                <Grid alignItems="center" container spacing={3} wrap="nowrap">
-                  <Grid item>
-                    <Avatar>
-                      <span className={alert.icon} />
-                    </Avatar>
-                  </Grid>
-                  <Grid item>
-                    <Typography component="h2" variant="h3">
-                      {alert.title}
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </ExpansionPanelSummary>
-              {alert.detail && (
-                <ExpansionPanelDetails>
-                  <Typography>{alert.detail}</Typography>
-                </ExpansionPanelDetails>
-              )}
-            </ExpansionPanel>
-          ))}
+          alerts.map(alert => <AlertPanel alert={alert} key={alert.title} />)}
       </Container>
     </Drawer>
   );
