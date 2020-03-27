815a7e3764334556c7942571904e03820a1a8a02
jacobvenable <jacob.venable@gmail.com>
2020-03-27 00:55:25 -0600

    feat: account for multiple alerts returned in <GlobalAlertBar/>

--- packages/upswyng-web/src/components/GlobalAlertBar.tsx
+++ packages/upswyng-web/src/components/GlobalAlertBar.tsx
@@ -3,8 +3,8 @@ import Alert from "@material-ui/lab/Alert";
 import AlertsDrawer from "./AlertsDrawer";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
-import ChevronRight from "@material-ui/icons/ChevronRight";
 import Container from "@material-ui/core/Container";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 import { TStatusFetch } from "@upswyng/upswyng-types";
 import { Theme } from "@material-ui/core/styles/createMuiTheme";
 import makeStyles from "@material-ui/styles/makeStyles";
@@ -14,7 +14,8 @@ const useAlertStyles = makeStyles((theme: Theme) => ({
   root: {
     background: "none",
     color: theme.palette.common.white,
-    padding: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
   },
   icon: {
     "&&": {
@@ -27,33 +28,38 @@ const GlobalAlertBar = () => {
   const [status, alerts] = useAlerts();
   const alertClasses = useAlertStyles();
   const [viewingAlerts, setViewingAlerts] = useState(false);
-  const alert = alerts?.length ? alerts[0] : null;
 
-  const toggleDrawer = () => setViewingAlerts(b => !b);
-
-  if (status !== TStatusFetch.STATUS_FETCH_SUCCESS || !alert) {
+  if (status !== TStatusFetch.STATUS_FETCH_SUCCESS || !alerts?.length) {
     return null;
   }
 
+  const toggleDrawer = () => setViewingAlerts(b => !b);
+  const [firstAlert] = alerts;
+  const alertButtonText = [
+    "View",
+    alerts.length,
+    alerts.length ? "Alerts" : "Alert",
+  ].reduce((acc, item) => (item ? `${acc} ${item}` : acc), "");
+
   return (
     <>
-      <Box bgcolor={alert.color || "primary.main"} color="common.white">
+      <Box bgcolor={firstAlert.color || "primary.main"} color="common.white">
         <Container>
           <Alert
             action={
               <Button
-                endIcon={<ChevronRight />}
                 onClick={toggleDrawer}
+                endIcon={<ExpandMore />}
                 size="small"
                 variant="text"
               >
-                See Alert
+                {alertButtonText}
               </Button>
             }
             classes={alertClasses}
-            icon={<span className={alert.icon} />}
+            icon={<span className={firstAlert.icon} />}
           >
-            {alert.title}
+            {firstAlert.title}
           </Alert>
         </Container>
       </Box>
