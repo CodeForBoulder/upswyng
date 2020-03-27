9494814bd137af95e92e3a21b186926ea103d461
jollyjerr <jollyjerr@gmail.com>
2020-03-27 08:57:28 -0600

    :recycle: switch to mui grid

--- packages/upswyng-web/src/components/NotFound.tsx
+++ packages/upswyng-web/src/components/NotFound.tsx
@@ -1,4 +1,5 @@
 import Container from "@material-ui/core/Container";
+import { Grid } from "@material-ui/core";
 import MuiLink from "@material-ui/core/Link";
 import PageBanner from "./PageBanner";
 import React from "react";
@@ -6,37 +7,30 @@ import { Link as RouterLink } from "react-router-dom";
 import SearchForm from "./SearchForm";
 import Typography from "@material-ui/core/Typography";
 import { colors } from "../App.styles";
-import createStyles from "@material-ui/core/styles/createStyles";
-import makeStyles from "@material-ui/core/styles/makeStyles";
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    marginTop: {
-      marginTop: "3.5rem",
-    },
-  })
-);
-
-const NotFound = () => {
-  const classes = useStyles();
-  return (
-    <Container>
-      <PageBanner color={colors.orangeDark} text={"Page not Found"} />
-      <Typography variant="h3">
-        Whoops! We&apos;re having trouble finding that page.
-      </Typography>
-      <div className={classes.marginTop}>
+const NotFound = () => (
+  <Container>
+    <PageBanner color={colors.orangeDark} text={"Page not Found"} />
+    <Grid container direction="column" spacing={4}>
+      <Grid item>
+        <Typography component="p" variant="h3">
+          Whoops! We&apos;re having trouble finding that page.
+        </Typography>
+      </Grid>
+      <Grid item>
         <SearchForm />
-      </div>
-      <Typography className={classes.marginTop}>
-        You can also go back{" "}
-        <MuiLink component={RouterLink} to="/">
-          home
-        </MuiLink>{" "}
-        to browse.
-      </Typography>
-    </Container>
-  );
-};
+      </Grid>
+      <Grid item>
+        <Typography>
+          You can also go back{" "}
+          <MuiLink component={RouterLink} to="/">
+            home
+          </MuiLink>{" "}
+          to browse.
+        </Typography>
+      </Grid>
+    </Grid>
+  </Container>
+);
 
 export default NotFound;
