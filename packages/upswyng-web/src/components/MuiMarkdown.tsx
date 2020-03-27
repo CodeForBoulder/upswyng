9d1694f55405a00b7f3e9f91801c6e6964cef2d5
jacobvenable <jacob.venable@gmail.com>
2020-03-26 23:26:30 -0600

    fix: commit missed <MuiMarkdown/>

---
+++ packages/upswyng-web/src/components/MuiMarkdown.tsx
@@ -0,0 +1,80 @@
+import ReactMarkdown, { NodeType } from "react-markdown";
+import Divider from "@material-ui/core/Divider";
+import Link from "@material-ui/core/Link";
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+type THeadingVariant = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+const asHeadingVariant = (headingLevel: number): THeadingVariant | null => {
+  switch (headingLevel) {
+    case 1:
+      return "h1";
+    case 2:
+      return "h2";
+    case 3:
+      return "h3";
+    case 4:
+      return "h4";
+    case 5:
+      return "h5";
+    case 6:
+      return "h6";
+    default:
+      return null;
+  }
+};
+
+interface RendererProps {
+  children: React.ReactNode;
+}
+
+interface HeadingRendererProps extends RendererProps {
+  level: number;
+}
+
+interface ListRendererProps extends RendererProps {
+  ordered: boolean;
+}
+
+const renderers: Record<string, React.ElementType> = {
+  emphasis: Typography,
+  heading: ({ children, level }: HeadingRendererProps) => {
+    const headingVariant = asHeadingVariant(level);
+    if (!headingVariant) {
+      return null;
+    }
+    return (
+      <Typography gutterBottom variant={headingVariant}>
+        {children}
+      </Typography>
+    );
+  },
+  link: Link,
+  list: ({ children, ordered }: ListRendererProps) => {
+    return (
+      <Typography component={ordered ? "ol" : "ul"} paragraph>
+        {children}
+      </Typography>
+    );
+  },
+  listItem: ({ children }: RendererProps) => (
+    <Typography component="li" paragraph>
+      {children}
+    </Typography>
+  ),
+  paragraph: ({ children }: RendererProps) => (
+    <Typography paragraph>{children}</Typography>
+  ),
+  thematicBreak: Divider,
+};
+
+interface Props {
+  markdown: string;
+}
+
+const MuiMarkdown = ({ markdown }: Props) => (
+  <ReactMarkdown renderers={renderers} source={markdown} />
+);
+
+export default MuiMarkdown;
