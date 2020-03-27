db577ac428bac99e07c2dcf11e25423fa28d06c5
Jeremiah Tabb <51095001+jollyjerr@users.noreply.github.com>
2020-03-27 09:06:05 -0600

    🧼 add "not found" component (#308)
    * :beers: add "not found" component
    
    * :recycle: switch to mui grid

---
+++ packages/upswyng-web/src/components/NotFound.tsx
@@ -0,0 +1,36 @@
+import Container from "@material-ui/core/Container";
+import { Grid } from "@material-ui/core";
+import MuiLink from "@material-ui/core/Link";
+import PageBanner from "./PageBanner";
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import SearchForm from "./SearchForm";
+import Typography from "@material-ui/core/Typography";
+import { colors } from "../App.styles";
+
+const NotFound = () => (
+  <Container>
+    <PageBanner color={colors.orangeDark} text={"Page not Found"} />
+    <Grid container direction="column" spacing={4}>
+      <Grid item>
+        <Typography component="p" variant="h3">
+          Whoops! We&apos;re having trouble finding that page.
+        </Typography>
+      </Grid>
+      <Grid item>
+        <SearchForm />
+      </Grid>
+      <Grid item>
+        <Typography>
+          You can also go back{" "}
+          <MuiLink component={RouterLink} to="/">
+            home
+          </MuiLink>{" "}
+          to browse.
+        </Typography>
+      </Grid>
+    </Grid>
+  </Container>
+);
+
+export default NotFound;
