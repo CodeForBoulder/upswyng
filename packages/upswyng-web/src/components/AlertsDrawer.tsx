fcdfcceb40f324fc6987bba21f02682052a245b3
Jacob <jacob.venable@gmail.com>
2020-03-31 22:11:53 -0600

    display alert bar (#309)
    * feat: create initial useAlerts hook
    
    * feat: update useAlerts to only return list of alerts
    
    * feat: add font awesome script to index.html
    
    * feat: create initial <GlobalAlertBar/>
    
    * feat: add placeholder 'Read More' button in <GlobalAlertBar/>
    
    * feat: add end icon to 'Read More' button in <GlobalAlertBar/>
    
    * fix: improper requested alerts in useAlerts
    
    * feat: create initial <AlertsDrawer/>
    
    * feat: separate alert panels from alert drawer
    
    * feat: add expand icon to alert panel
    
    * feat: add react-markdown as depedency
    
    * feat: implement initial markdown to MUI converter
    
    * fix: commit missed <MuiMarkdown/>
    
    * feat: add additional renders to <MuiMarkdown/>
    
    * feat: update the first listed alert to be open by default
    
    * feat: account for multiple alerts returned in <GlobalAlertBar/>
    
    * feat: add button to hide the alerts drawer
    
    * feat: style AlertsDrawer
    
    * feat: make entire alert bar clickable
    
    * feat: conditionally render the expand icon in <AlertPanel/>
    
    * feat: add indicator when there are no extra details on alert
    
    * feat: update useAlerts to request every 5 minutes
    
    * feat: sort alerts by start date in <GlobalAlertBar/>
    
    * feat: conditionally render number of alerts in <GlobalAlertBar/>
    
    * feat: remove unnecessary type from <MuiMarkdown/>
    
    * chore: remove unnecessary comment from <AlertPanel/>
    
    * fix: update use of TAlertFull type to TAlert

---
+++ packages/upswyng-web/src/components/AlertsDrawer.tsx
@@ -0,0 +1,62 @@
+import Drawer, { DrawerProps } from "@material-ui/core/Drawer";
+import AlertPanel from "./AlertPanel";
+import CloseIcon from "@material-ui/icons/Close";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import React from "react";
+import { TAlert } from "@upswyng/upswyng-types";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+import Typography from "@material-ui/core/Typography";
+import makeStyles from "@material-ui/styles/makeStyles";
+
+interface Props extends DrawerProps {
+  alerts: TAlert[] | null;
+  toggleDrawer: () => void;
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  drawerPaper: {
+    backgroundColor: theme.palette.grey[900],
+    paddingBottom: theme.spacing(5),
+    paddingTop: theme.spacing(5),
+  },
+}));
+
+const AlertsDrawer = ({ alerts, toggleDrawer, ...drawerProps }: Props) => {
+  const classes = useStyles();
+  return (
+    <Drawer
+      anchor="top"
+      {...drawerProps}
+      PaperProps={{ className: classes.drawerPaper }}
+    >
+      <Container>
+        <Grid container direction="column" spacing={4}>
+          <Grid item>
+            <Typography variant="h1">Alerts</Typography>
+          </Grid>
+          {alerts && (
+            <Grid item>
+              {alerts.map((alert, i) => (
+                <AlertPanel
+                  alert={alert}
+                  defaultExpanded={i === 0}
+                  key={alert.title}
+                />
+              ))}
+            </Grid>
+          )}
+          <Grid container justify="flex-end" item>
+            <IconButton onClick={toggleDrawer}>
+              <Typography variant="srOnly">close alerts</Typography>
+              <CloseIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Container>
+    </Drawer>
+  );
+};
+
+export default AlertsDrawer;
