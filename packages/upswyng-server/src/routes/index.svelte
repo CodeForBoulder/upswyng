fb34465927990ee3d914f4682c06f5baae2c9f13
Jacob <jacob.venable@gmail.com>
2020-04-19 17:28:58 -0600

    provider portal home page updates (#321)
    * chore: update resources to Service Providers in navigation
    
    * feat: conditionally render schedule entry based on always open
    
    * style: wrap navbar content in container
    
    * feat: update upswyng logo with taglined version
    
    * chore: update alt text of home screen logo
    
    * feat: update provider portal content
    
    * feat: add hidden label to resource search
    
    * chore: update instances of resource to provider in search
    
    * chore: update content on home page
    
    * Revert "feat: conditionally render schedule entry based on always open"
    
    This reverts commit 7f646343dda33ecad7f46ca87360c272faddf255.
    
    * feat: add CTA to add listing to provider home
    
    * feat: remove previous home page search

--- packages/upswyng-server/src/routes/index.svelte
+++ packages/upswyng-server/src/routes/index.svelte
@@ -12,63 +12,64 @@
     width: 100%;
     margin-right: 6em;
   }
-
-  .hero-container {
-    display: flex;
-    align-items: center;
-  }
-
-  .hero-image-container {
-    flex: 3;
-    margin-right: 3em;
-  }
-
-  .hero-container p {
-    flex: 2;
-  }
-
-  .hero-tag {
-    color: white;
-    line-height: 1.25em;
-  }
-
-  .visit-web-app {
-    display: block;
-    margin: 0 auto;
-  }
 </style>
 
 `
 <svelte:head>
-  <title>Upswyng</title>
+  <title>Upswyng: Provider Portal</title>
 </svelte:head>
 
-<section class="hero">
+<section class="hero is-dark">
   <div class="hero-body upswyng-intro">
-    <div class="container hero-container">
-      <div class="hero-image-container">
-        <img alt="Upswyng: Resource within reach" src="upswyng_light.svg" />
+    <div class="container">
+      <div class="columns is-variable is-6 is-vcentered">
+        <div class="column">
+          <img alt="upswyng: Resources within reach" src="upswyng_light.svg" />
+        </div>
+        <div class="column">
+          <h1 class="title">Upswyng Provider Portal</h1>
+          <p class="subtitle">
+            Welcome to the Provider’s Portal. This is the place to update your
+            organization’s information so it’s the latest and most accurate for
+            the at-risk community.
+          </p>
+        </div>
       </div>
-      <p class="title hero-tag">
-        Upswyng is helping people in Boulder access the resources they need.
-      </p>
     </div>
   </div>
 </section>
 <section class="section">
   <div class="container">
-    <a
-      href="https://upswyng.netlify.app"
-      class="button is-primary visit-web-app">
-      Visit the Web App
-    </a>
-  </div>
-  <div class="container">
-    <h2 class="subtitle">Search for a Resource, Just for Mike</h2>
-    <ResourceSearch
-      action="view"
-      on:resourceClick={({ detail: resourceId }) => {
-        goto(`/resource/${resourceId}`);
-      }} />
+    <div class="columns is-variable is-6">
+      <div class="column content">
+        <h2 class="is-size-4 has-text-weight-semibold">Find Your Listing</h2>
+        <ResourceSearch
+          action="view"
+          on:resourceClick={({ detail: resourceId }) => {
+            goto(`/resource/${resourceId}`);
+          }} />
+        <p>
+          We have made an easy and intuitive portal for you to make changes. All
+          changes are subject to confirmation before going live. Just update and
+          save as a draft and we will do the rest.
+        </p>
+        <a href="/login" class="button is-primary">Login to Add Yours</a>
+      </div>
+      <div class="column">
+        <aside class="message is-medium">
+          <h2 class="message-header">Looking for the public app?</h2>
+          <div class="columns is-multiline message-body">
+            <p class="column is-full">
+              See how the at-risk community can find your offerings.
+            </p>
+            <div class="column">
+              <a href="https://upswyng.netlify.com" class="button is-primary">
+                Visit the Web App
+              </a>
+            </div>
+          </div>
+        </aside>
+      </div>
+    </div>
   </div>
 </section>
