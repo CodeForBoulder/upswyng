111deb34ff9bc6c051773471e3410232f3ea7bf6
jacobvenable <jacob.venable@gmail.com>
2020-04-19 23:24:05 -0600

    feat: move resource breadcrumbs to separate component

--- packages/upswyng-server/src/components/ResourceEditor.svelte
+++ packages/upswyng-server/src/components/ResourceEditor.svelte
@@ -73,30 +73,6 @@
   }));
 </script>
 
-<style>
-  nav:not(:last-of-type) {
-    margin-bottom: 0;
-  }
-</style>
-
-{#each resource.subcategories || [] as subcategory}
-  <nav class="breadcrumb has-bullet-separator" aria-label="breadcrumbs">
-    <ul>
-      <li>
-        <a href={`/category/${subcategory.parentCategory.stub}`}>
-          {subcategory.parentCategory.name}
-        </a>
-      </li>
-      <li>
-        <a href={`/subcategory/${subcategory.stub}`}>{subcategory.name}</a>
-      </li>
-      <li class="is-active">
-        <!-- svelte-ignore a11y-invalid-attribute -->
-        <a href="#" aria-current="page">{resource.name}</a>
-      </li>
-    </ul>
-  </nav>
-{/each}
 <div class="content">
   <div class="box" class:has-ribbon={resource.deleted}>
     {#if resource.deleted}
