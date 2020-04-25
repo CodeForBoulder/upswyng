b951c89e51b262d432b824749e4a98fa1c5b88be
jollyjerr <jollyjerr@gmail.com>
2020-04-25 16:55:41 -0600

    :children_crossing: improve hotline page

--- packages/upswyng-web/src/components/HotlineCard.tsx
+++ packages/upswyng-web/src/components/HotlineCard.tsx
@@ -1,11 +1,12 @@
 import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 
 const initiatePhoneCall = (phoneNumber: string): void => {
-  window.open(`tel:${phoneNumber}`);
+  window.open(`tel:${phoneNumber.replace(/[-]/g, "")}`);
 };
 
 interface Props {
@@ -17,24 +18,31 @@ interface Props {
 const HotlineCard = ({ name, description, phone }: Props) => {
   const [hideText, setHideText] = React.useState(true);
   return (
-    <ListItem button>
+    <ListItem>
       <ListItemText
-        onClick={() => initiatePhoneCall(phone)}
         primary={name}
         secondary={
           <>
-            <Typography>{phone}</Typography>
+            <Link
+              underline="always"
+              onClick={() => initiatePhoneCall(phone)}
+              color="secondary"
+              style={{ cursor: "pointer" }}
+            >
+              {phone}
+            </Link>
+            <Button
+              onClick={() => setHideText(!hideText)}
+              size="small"
+              color="secondary"
+              style={{ float: "right" }}
+            >
+              {hideText ? "More" : "Less"}
+            </Button>
             <Typography noWrap={hideText}>{description}</Typography>
           </>
         }
       />
-      <Button
-        onClick={() => setHideText(!hideText)}
-        size="small"
-        color="secondary"
-      >
-        {hideText ? "More" : "Less"}
-      </Button>
     </ListItem>
   );
 };
