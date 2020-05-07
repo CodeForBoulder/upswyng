e8dc643f5fc8fd9f79f97eaa39d354c6b339845e
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2020-05-06 22:23:07 -0600

    Teardown redis

--- packages/upswyng-server/src/bin/e2e_teardown.sh
+++ packages/upswyng-server/src/bin/e2e_teardown.sh
@@ -6,9 +6,16 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # get the pid of the mongodb instance
 read -r MONGODB_PID <"${DIR}/../../cypress/.mongodbpid"
-
 # kill it
 kill $MONGODB_PID
 
+# get the pid of the mongodb instance
+read -r REDIS_PID <"${DIR}/../../cypress/.redispid"
+# kill it
+kill $REDIS_PID
+
 # clean up artifacts from start
-rm "${DIR}/../../cypress/.mongodbpid" "${DIR}/../../cypress/.mongodburi"
+rm "${DIR}/../../cypress/.mongodbpid" \
+    "${DIR}/../../cypress/.mongodburi" \
+    "${DIR}/../../cypress/.redispid" \
+    "${DIR}/../../cypress/.redisuri"
