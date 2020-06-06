2d2909cdcb7880f1f9b47c1e4272d8d5f8d9e278
jacobvenable <jacob.venable@gmail.com>
2020-06-06 13:28:22 -0600

    feat: update subcategories endpoint to sort by cat then subcat name

--- packages/server/src/routes/api/subcategories.ts
+++ packages/server/src/routes/api/subcategories.ts
@@ -3,8 +3,16 @@ import Subcategory from "../../models/Subcategory";
 export async function get(_req, res) {
   try {
     const subcategories = await Subcategory.getSubcategoryList();
+    const collator = new Intl.Collator("en");
+    const sortedSubcategories = subcategories.sort(
+      (subCatA, subCatB) =>
+        collator.compare(
+          subCatA.parentCategory.name,
+          subCatB.parentCategory.name
+        ) || collator.compare(subCatA.name, subCatB.name)
+    );
     res.writeHead(200, { "Content-Type": "application/json" });
-    return res.end(JSON.stringify({ subcategories }));
+    return res.end(JSON.stringify({ subcategories: sortedSubcategories }));
   } catch (e) {
     console.error(e);
     res.writeHead(500).end("Error fetching subcategories");
