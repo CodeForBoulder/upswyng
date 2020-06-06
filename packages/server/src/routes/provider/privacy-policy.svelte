185db0b3ebf2287592ed4c8b794716c105bda918
jacobvenable <jacob.venable@gmail.com>
2020-06-06 12:11:52 -0600

    feat: add privacy policy provider portal page

---
+++ packages/server/src/routes/provider/privacy-policy.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { PrivacyPolicyContent } from "@upswyng/common";
+  import insane from "insane";
+
+  $: sanitizedPrivacyPolicyContent = insane(PrivacyPolicyContent, {
+    ...insane.defaults,
+    allowedAttributes: {
+      ...insane.defaults.allowedAttributes,
+      iframe: [],
+    },
+  });
+</script>
+
+<svelte:head>
+  <title>Privacy Policy</title>
+</svelte:head>
+
+<section class="section">
+  <div class="container">
+    <div class="content">
+      {@html sanitizedPrivacyPolicyContent}
+    </div>
+  </div>
+</section>
