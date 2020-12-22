2eae3b3cde5a8c62274c0dfaf875323a24f0c14a
Jacob <jacob.venable@gmail.com>
2020-12-21 20:25:43 -0700

    feat: add data deletion instructions page (#432)
    * feat: add data deletion instructions page
    
    * chore: update data deletion content

---
+++ packages/server/src/routes/provider/data-deletion.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+  <title>Requesting Data Deletion</title>
+</svelte:head>
+
+<section class="section">
+  <div class="container">
+    <div class="content">
+      <h1 class="title">Requesting Data Deletion</h1>
+      <p>If you shared information with UpSwyng that you would like to delete from our records, please contact us at <a href="mailto:info@upswyng.org">info@upswyng.org</a>.</p>
+      <div class="notification is-primary">
+        <p>When contacting us, please do so using the email associated with your UpSwyng account. This will assist in verifying your request and expedite the process.</p>
+      </div>
+      <p>For more information about how your data is used, please reference our <a href="/provider/privacy-policy">Privacy Policy</a>.</p>
+    </div>
+  </div>
+</section>
