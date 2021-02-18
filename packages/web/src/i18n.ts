d39b87514361bf927636fd8c8de5cc5d5b400aa9
paspheeris <pa.spheeris@gmail.com>
2021-02-18 12:29:28 -0700

    Move translations to separate json files

--- packages/web/src/i18n.ts
+++ packages/web/src/i18n.ts
@@ -1,3 +1,5 @@
+import en from "@upswyng/common/src/i18n/en.json";
+import es from "@upswyng/common/src/i18n/es.json";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
@@ -5,44 +7,14 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources: {
-      en: {
-        home: {
-          food: "Food",
-          shelter: "Shelter",
-          hygiene: "Hygiene",
-          transit: "Transit",
-          resources: "Resources",
-          hotlines: "Hotlines",
-          health: "Health",
-          wifi: "Wifi",
-          jobTraining: "Job Training",
-          socialServices: "Social Services",
-          coordinatedEntry: "Coordinated Entry",
-        },
-      },
-      es: {
-        home: {
-          food: "Comida",
-          shelter: "Abrigo",
-          hygiene: "Higiene",
-          transit: "Tránsito",
-          resources: "Recursos",
-          hotlines: "Líneas Directas",
-          health: "Salud",
-          wifi: "Wifi",
-          jobTraining: "Formación Profesional",
-          socialServices: "Servicios Sociales",
-          coordinatedEntry: "Entrada Coordinada",
-        },
-      },
+      en,
+      es,
     },
     debug: false, // change to true for development
     lng: "en",
     fallbackLng: "en",
     whitelist: ["en", "es"],
-
     keySeparator: ".",
-
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
