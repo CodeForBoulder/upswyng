db2ab60b7ab20751e091341979ca665bbc489ef5
jacobvenable <jacob.venable@gmail.com>
2021-03-22 16:23:34 -0600

    chore: convert translations to ts

---
+++ packages/common/src/i18n/en.ts
@@ -0,0 +1,188 @@
+export default {
+  categories: {
+    food: "Food",
+    shelter: "Shelter",
+    hygiene: "Hygiene",
+    transit: "Transit",
+    resources: "Resources",
+    hotlines: "Hotlines",
+    health: "Health",
+    wifi: "Wifi",
+    jobTraining: "Job Training",
+    socialServices: "Social Services",
+  },
+  coordinatedEntry: {
+    mainNotice:
+      "All single homeless adults must go through the Coordinated Entry process to receive services from Boulder County or the cities of Boulder or Longmont. After meeting with a staff person and going through a short screening, clients will be referred to the most appropriate service depending on their needs.",
+    sixMonthsNotice:
+      "Note you must have been in Boulder County for 6 months before applying.",
+    earlyApplicationNotice:
+      "If you apply before 6 months, you will be barred from any services.",
+  },
+  glossary: {
+    coordinatedEntry: "Coordinated Entry",
+    more: "More",
+    less: "Less",
+    comments: "Comments",
+    send: "Send",
+    back: "Back",
+    weekdays: {
+      sunday: "Sunday",
+      monday: "Monday",
+      tuesday: "Tuesday",
+      wednesday: "Wednesday",
+      thursday: "Thursday",
+      friday: "Friday",
+      saturday: "Saturday",
+    },
+  },
+  hotlines: {
+    nationalSuicidePreventionLifeline: {
+      name: "National Suicide Prevention Lifeline",
+      description:
+        "The National Suicide Prevention Lifeline provides free and confidential emotional support to people in suicidal crisis or emotional distress 24 hours a day, 7 days a week, across the United States. The Lifeline is comprised of a national network of over 160 local crisis centers, combining custom local care and resources with national standards and best practices.",
+    },
+    nationalDomesticViolenceHotline: {
+      name: "National Domestic Violence Hotline",
+      description:
+        "Operating around the clock, seven days a week, confidential and free of cost, the National Domestic Violence Hotline provides lifesaving tools and immediate support to enable victims to find safety and live lives free of abuse. Callers to The Hotline at 1-800-799-SAFE (7233) can expect highly trained, experienced advocates to offer compassionate support, crisis intervention information and referral services in over 170 languages. Our phone and chat services are available to anyone who has been affected by relationship abuse, including those who are currently in abusive relationships, those who are working to heal, friends or family of victims and survivors and anyone in the community who has questions about domestic violence.",
+    },
+    veteransCrisisLine: {
+      name: "Veterans Crisis Line",
+      description:
+        "The Veterans Crisis Line connects Veterans in crisis and their families and friends with qualified, caring Department of Veterans Affairs responders through a confidential toll-free hotline, online chat, or text.  Veterans and their loved ones can call 1-800-273-8255 and Press 1, chat online, or send a text message to 838255 to receive confidential support 24 hours a day, 7 days a week, 365 days a year. Support for deaf and hard of hearing individuals is available.",
+    },
+    nationalChildAbuseHotline: {
+      name: "National Child Abuse Hotline",
+      description:
+        "The Childhelp National Child Abuse Hotline 1-800-4-A-CHILD (1-800-422-4453) is dedicated to the prevention of child abuse. Serving the United States, its territories, and Canada, the hotline is staffed 24 hours a day, 7 days a week with professional crisis counselors who, through interpreters, can provide assistance in over 170 languages. The hotline offers crisis intervention, information, literature, and referrals to thousands of emergency, social service, and support resources. All calls are confidential.",
+    },
+    nationalRunawaySafeline: {
+      name: "National Runaway Safeline",
+      description:
+        "For Youth and Teens, The National Runaway Safeline (NRS) is here to listen whether you are thinking of running away or already have. Our services are confidential and nonjudgmental. For Parents and Guardians, NRS can offer you support and help connect you to the right resources for your family. Whether you’re having trouble talking to your child or your child has already run, we can help. We will listen and assist you in developing a plan.",
+    },
+    glbtNationalHotline: {
+      name: "GLBT National Hotline",
+      description:
+        "The Gay, Lesbian, Bisexual and Transgender (GLBT) National Hotline provides telephone, online private one-to-one chat and email peer-support, as well as factual information and local resources for cities and towns across the United States. All of our services are free and confidential. We speak with callers of all ages about coming-out issues, relationship concerns, bullying, workplace issues, HIV/AIDS anxiety and safer-sex information, and lots more! Utilizing a diverse group of GLBT volunteers, we operate two national hotlines, the GLBT National Hotline(1-888-843-4564) and the GLBT National Youth Talkline(1-800-246-7743), as well as private, volunteer one-to-one online chat, that helps both youth and adults.  Email: help@GLBThotline.org",
+    },
+    nationalHumanTraffickingResourceCenter: {
+      name: "National Human Trafficking Resource Center",
+      description:
+        "The National Human Trafficking Resource Center (NHTRC) is a national anti-trafficking hotline and resource center serving victims and survivors of human trafficking and the anti-trafficking community in the United States. The toll-free hotline is available to answer calls from anywhere in the country, 24 hours a day, 7 days a week, every day of the year in more than 200 languages. The NHTRC can also be accessed by emailing nhtrc@polarisproject.org. Our mission is to provide human trafficking victims and survivors with access to critical support and services to get help and stay safe, and to equip the anti-trafficking community with the tools to effectively combat all forms of human trafficking. We offer round-the-clock access to a safe space to report tips, seek services, and ask for help.",
+    },
+    nationalSexualAssultHotline: {
+      name: "National Sexual Assult Hotline",
+      description:
+        "RAINN (Rape, Abuse and Incest National Network) is the nation's largest anti-sexual violence organization. RAINN created and operates the National Sexual Assault Hotline (800.656.HOPE, online.rainn.org y rainn.org/es) in partnership with more than 1,000 local sexual assault service providers across the country and operates the DoD Safe Helpline for the Department of Defense. RAINN also carries out programs to prevent sexual violence, help victims, and ensure that perpetrators are brought to justice.",
+    },
+    transLifeline: {
+      name: "Trans Lifeline",
+      description:
+        "This line is primarily for transgender people experiencing a crisis. This includes people who may be struggling with their gender identity and are not sure that they are transgender. While our goal is to prevent self harm, we welcome the call of any transgender person in need. We will do our very best to connect them with services that can help them meet that need. If you are not sure whether you should call or not, then please call us. Our hotline is staffed by the true experts on transgender experience, transgender people themselves. Our volunteers are all trans identified and educated in the range of difficulties transgender people experience. Our volunteers are dedicated to improving the lives of transgender people. Additionally, our operators will only call emergency services with your expressed consent.",
+    },
+    substanceAbuseAndMentalHealthServicesAdministration: {
+      name: "Substance Abuse and Mental Health Services Administration",
+      description:
+        "SAMHSA’s National Helpline (also known as the Treatment Referral Routing Service) is a confidential, free, 24-hour-a-day, 365-day-a-year, information service, in English and Spanish, for individuals and family members facing mental and/or substance use disorders. This service provides referrals to local treatment facilities, support groups, and community-based organizations. Callers can also order free publications and other information.",
+    },
+    nationalHotlineForCrimeVictims: {
+      name: "National Hotline for Crime Victims",
+      description:
+        "The VictimConnect Resource Center is a place for crime victims to learn about their rights and options confidentially and compassionately. VictimConnect serves victims of any crime in the United States. Whether by chat or phone, our specialists assist victims in locating appropriate national, local, or online referrals. VictimConnect also has a special focus on populations, crimes, and topics that are generally underrepresented or underserved in victim services.",
+    },
+    nationalDisasterDistressHelpline: {
+      name: "National Disaster Distress Helpline",
+      description:
+        "The Disaster Distress Helpline is a national hotline dedicated to providing year-round immediate crisis counseling for people who are experiencing emotional distress related to any natural or human-caused disaster. This toll-free, multilingual, and confidential crisis support service is available to all residents in the United States and its territories. Stress, anxiety, and other depression-like symptoms are common reactions after a disaster.",
+    },
+    unitedWay: {
+      name: "United Way",
+      description:
+        "Every day thousands of people across North America turn to 2-1-1 for information and support—whether financial, domestic, health or disaster-related. 2-1-1 is a free, confidential referral and information helpline and website that connects people from all communities and of all ages to the essential health and human services they need, 24 hours a day, seven days a week.",
+    },
+  },
+  map: {
+    directions: "Directions",
+    directionsByBus: "Directions by bus",
+    directionsByBicycle: "Directions by bicycle",
+    directionsByCar: "Directions by car",
+    directionsByWalking: "Directions by walking",
+  },
+  notFound: {
+    pageTitle: "Page not Found",
+    message: "Whoops! We're having trouble finding that page.",
+    goHome: "You can also go back <MuiLink>home</MuiLink> to browse",
+  },
+  reportIssue: {
+    title: "Report an Issue",
+    reportFor: "Report an issue for:",
+    address: "The address is wrong",
+    phone: "The phone number is wrong",
+    website: "The website link is wrong",
+    schedule: "The schedule is wrong",
+    directions: "The directions are wrong",
+    noFeedbackSubmitted:
+      "No feedback to submit. Please check desired boxes, enter comments, and try again.",
+    problemSendingFeedback:
+      "There was a problem sending your feedback. Please try again.",
+    submissionSuccess:
+      "The problem has been submitted and will be reviewed. Thank you for your feedback!",
+  },
+  resource: {
+    address: "Address",
+    phoneNumber: "Phone Number",
+    website: "Website",
+    hoursOfOperation: "Hours of Operation",
+    reportAProblem: "Report a Problem",
+    services: "Services",
+  },
+  searchInput: {
+    placeholder: "Search - What are your needs today?",
+  },
+  subcategories: {
+    all: "All",
+    meals: "Meals",
+    foodPantries: "Food Pantries",
+    emergency: "Emergency",
+    family: "Family",
+    youth: "Youth",
+    abused: "Abused",
+    pregnant: "Pregnant",
+    temporary: "Temporary",
+    transitional: "Transitional",
+    safeParking: "Safe Parking",
+    restrooms: "Restrooms",
+    showers: "Showers",
+    waterFountains: "Water Fountains",
+    feminineProducts: "Feminine Products",
+    busPasses: "Bus Passes",
+    bicycle: "Bicycle",
+    outdoorGear: "Outdoor Gear",
+    clothing: "Clothing",
+    shoes: "Shoes",
+    legalHelp: "Legal Help",
+    pets: "Pets",
+    laundry: "Laundry",
+    hairCare: "Hair Care",
+    homeGoods: "Home Goods",
+    hospital: "Hospital",
+    clinics: "Clinics",
+    mental: "Mental",
+    dental: "Dental",
+    pharmacies: "Pharmacies",
+    vision: "Vision",
+    addiction: "Addiction",
+    freeWifi: "Free Wifi",
+    publicComputer: "Public Computer",
+    charging: "Charging",
+    readyToWork: "Ready to Work",
+    craigsList: "CraigsList",
+    tempAgency: "Temp Agency",
+    careerCounseling: "Career Counseling",
+    healthAndHumanServices: "Health and Human Services",
+    foodStamps: "Food Stamps",
+    socialSecurity: "Social Security",
+  },
+};
