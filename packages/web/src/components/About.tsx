097e14e09d0170b6cdc791cd1a1dcbb3963a1219
jacobvenable <jacob.venable@gmail.com>
2021-03-23 18:20:17 -0600

    feat: translate about page content to spanish

--- packages/web/src/components/About.tsx
+++ packages/web/src/components/About.tsx
@@ -4,15 +4,11 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 const About = () => {
-  const { i18n, t } = useTranslation("about");
-
-  const changeLng = (lng: string) => i18n.changeLanguage(lng);
+  const { t } = useTranslation("about");
 
   return (
     <Container>
       <MuiHtml html={t("content")} />
-      <button onClick={() => changeLng("en")}>Switch en language</button>
-      <button onClick={() => changeLng("es")}>Switch es language</button>
     </Container>
   );
 };
