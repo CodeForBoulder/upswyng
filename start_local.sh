eeeadc12c0554ac97063d8f3a730ef1088aafa6c
jollyjerr <jollyjerr@gmail.com>
2021-04-15 21:01:55 -0600

    add start:local script

---
+++ start_local.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+COMMON_PATH="${DIR}/packages/common"
+CLIENT_PATH="${DIR}/packages/web"
+SERVER_PATH="${DIR}/packages/server"
+
+# Install deps
+yarn
+
+# Check for local packages; build them if they do not exist
+if ! [ -f "${COMMON_PATH}/dist/index.js" ]; then
+  yarn build:web
+fi
+
+# Check for .env files; fill them in if they do not exist
+if ! [ -f "${CLIENT_PATH}/.env" ]; then
+  cp "${CLIENT_PATH}/.env.local.example" "${CLIENT_PATH}/.env"
+fi
+
+if ! [ -f "${SERVER_PATH}/.env" ]; then
+  cp "${SERVER_PATH}/.env.example" "${SERVER_PATH}/.env"
+fi
+
+# Check for running docker network; start it up if it does not exist
+docker top upswyng-dev-db > /dev/null 2>&1 || yarn build:local-db
+
+# Start development servers for server and web packages concurrently
+concurrently -n server,web -c yellow,blue "yarn workspace @upswyng/server dev" "yarn workspace @upswyng/web start"
