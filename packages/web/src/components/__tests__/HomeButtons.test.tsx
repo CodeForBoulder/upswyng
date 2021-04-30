a224c1681b5ac0573a59b2a911793ae1588fa727
Jeremiah Tabb <51095001+jollyjerr@users.noreply.github.com>
2021-04-29 18:28:47 -0600

    add i18next mocks to tests (#470)

--- packages/web/src/components/__tests__/HomeButtons.test.tsx
+++ packages/web/src/components/__tests__/HomeButtons.test.tsx
@@ -8,6 +8,17 @@ jest.mock("../HomeLink", () => ({
     <div>{children}</div>
   ),
 }));
+jest.mock("react-i18next", () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => ({})),
+      },
+    };
+  },
+}));
 
 describe("<HomeButtons/>", () => {
   const routerLinkTexts = routerLinkButtons.map(
