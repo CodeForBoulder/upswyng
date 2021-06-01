5e4bae21ee8d21f808febdc88ae9606b37b4df99
jacobvenable <jacob.venable@gmail.com>
2021-05-31 20:56:33 -0600

    fix: console warning on home page

--- packages/web/src/components/HomeLink.tsx
+++ packages/web/src/components/HomeLink.tsx
@@ -1,29 +1,20 @@
-import { THomeButtonAnchor, THomeButtonRouterLink } from "../webTypes";
-
 import { Link } from "react-router-dom";
 import React from "react";
+import { THomeButtonRouterLink } from "../webTypes";
 
 interface HomeLinkPropsBase {
   children: React.ReactElement | React.ReactElement[];
 }
 
-type HomeRouterLinkProps = HomeLinkPropsBase & THomeButtonRouterLink;
-type HomeAnchorProps = HomeLinkPropsBase &
-  THomeButtonAnchor &
-  React.HTMLProps<HTMLAnchorElement>;
+type HomeLink = HomeLinkPropsBase & THomeButtonRouterLink;
 
-export const HomeRouterLink = (props: HomeRouterLinkProps) => {
-  const { children, linkProps, ...rest } = props;
+const HomeLink = (props: HomeLink) => {
+  const { children, linkProps } = props;
   return (
-    <Link {...linkProps} {...rest}>
+    <Link style={{ textDecoration: "none" }} {...linkProps}>
       {children}
     </Link>
   );
 };
 
-export const HomeAnchorLink = (props: HomeAnchorProps) => {
-  const { children, ...rest } = props;
-  return <a {...rest}>{children}</a>;
-};
-
-export default HomeRouterLink;
+export default HomeLink;
