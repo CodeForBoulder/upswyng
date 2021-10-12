926234710e6bb86159c617320aeee5c03bf29a7d
dannyirwin <dirwin123@gmail.com>
2021-10-12 12:35:34 -0600

    resource filter moved to resource endpoint

--- packages/server/src/routes/api/resources/index.ts
+++ packages/server/src/routes/api/resources/index.ts
@@ -1,13 +1,53 @@
 import Resource, { resourceDocumentToResource } from "../../../models/Resource";
+import Category from "../../../models/Category";
 import { ObjectId } from "bson";
+import { TCategory } from "@upswyng/types";
+import { TResource } from "@upswyng/types";
+import { TSubcategory } from "@upswyng/types";
 
 export async function get(_req, res, _next) {
   try {
     const idParam = _req.query.id;
-    const resourceIds = idParam?.split(",")?.map(ObjectId.createFromHexString);
-    const resourceDocuments = resourceIds
-      ? await Resource.getByResourceIds(resourceIds)
-      : await Resource.getAll();
+    const resourceIds = idParam?.split(",") || [];
+    const categoryFilters = _req.query?.categories?.split(",") || [];
+
+    const getCategories = async () => {
+      return Promise.all(
+        categoryFilters.map(async (categoryName: string) => {
+          const stub = categoryName.toLowerCase().replace(/\s/, "_"); //TODO: how will two word query params work?
+          return await Category.getByStub(stub);
+        })
+      );
+    };
+
+    const categories = await getCategories();
+    const resourceIdsFromCategories = [
+      ...new Set(
+        categories
+          .map((category: TCategory) => {
+            return category?.subcategories.map((subcategory: TSubcategory) => {
+              return subcategory?.resources.map((resource: TResource) =>
+                resource.resourceId.toString()
+              );
+            });
+          })
+          .flat(2)
+      ),
+    ];
+
+    const allIds =
+      categories.length > 0 && resourceIds.length > 0
+        ? resourceIds.filter((id: string) => {
+            return resourceIdsFromCategories.includes(id);
+          })
+        : [...new Set([...resourceIds, ...resourceIdsFromCategories])];
+    const allResourceIds = allIds.map(ObjectId.createFromHexString);
+
+    const resourceDocuments =
+      allResourceIds.length > 0
+        ? await Resource.getByResourceIds(allResourceIds)
+        : await Resource.getAll();
+
     const resources = await Promise.all(
       resourceDocuments.map(resourceDocumentToResource)
     );
