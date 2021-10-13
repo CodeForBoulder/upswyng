1ec17b9a1632eab6773e3e4cab387e6e76fab3a8
dannyirwin <dirwin123@gmail.com>
2021-10-13 11:39:46 -0600

    frontend consumes new /resources/ endpoint

--- packages/web/src/components/useResourcesByCategory.tsx
+++ packages/web/src/components/useResourcesByCategory.tsx
@@ -1,63 +1,30 @@
 import { TResource } from "@upswyng/types";
-import { TResourcesByCategoryPayload } from "../webTypes";
+import { TResourcePayload } from "../webTypes";
 import apiClient from "../utils/apiClient";
 import { useQuery } from "react-query";
 
 const getResourcesByCategory = async (
   _queryKey: string,
   params: { category: string }
-): Promise<TResourcesByCategoryPayload> => {
-  const { data } = await apiClient.get<TResourcesByCategoryPayload>(
-    `/category/${params.category}`
+): Promise<TResourcePayload> => {
+  const { data } = await apiClient.get<TResourcePayload>(
+    `/resources/?categories=${params.category}`
   );
 
-  if (!data.category) {
+  if (!data.resources) {
     throw new Error("no category found in resources by category response");
   }
 
-  const {
-    category: { subcategories },
-  } = data;
-  if (!(subcategories || []).length) {
-    throw new Error(
-      "no sub-categories found in resources by category response"
-    );
-  }
+  // const {
+  //   category: { subcategories },
+  // } = data;
+  // if (!(subcategories || []).length) {
+  //   throw new Error(
+  //     "no sub-categories found in resources by category response"
+  //   );
+  // }
 
   return data;
-};
-
-const getUniqueFlattenedResources = (
-  payload?: TResourcesByCategoryPayload
-): TResource[] | undefined => {
-  const subcategories = payload?.category?.subcategories;
-  if (!subcategories || !subcategories.length) {
-    return;
-  }
-
-  const uniqueResources = subcategories.reduce<TResource[]>(
-    (categoryResources, subcategory) => {
-      const { resources: subcategoryResources } = subcategory;
-      if (!subcategoryResources || !subcategoryResources.length) {
-        return categoryResources;
-      }
-
-      const uniqueSubcategoryResources = categoryResources.length
-        ? subcategoryResources.filter(
-            resource =>
-              !categoryResources.find(
-                categoryResource =>
-                  categoryResource.resourceId === resource.resourceId
-              )
-          )
-        : subcategoryResources;
-
-      return categoryResources.concat(uniqueSubcategoryResources);
-    },
-    []
-  );
-
-  return uniqueResources;
 };
 
 const useResourcesByCategory = (
@@ -73,7 +40,7 @@ const useResourcesByCategory = (
       staleTime: 900000, // 15 min
     }
   );
-  const resources = getUniqueFlattenedResources(data);
+  const resources = data?.resources;
 
   return { data: resources, status };
 };
