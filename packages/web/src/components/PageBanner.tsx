88fd358a78dab8edb55492fa1c12c878d8eda1bf
Alpha Bah <alphabah@Alphas-MacBook-Pro.local>
2021-12-14 18:24:07 -0700

    Please add banner style updates #272

--- packages/web/src/components/PageBanner.tsx
+++ packages/web/src/components/PageBanner.tsx
@@ -2,9 +2,10 @@ import BackButton from "./BackButton";
 import BannerColorContext from "./BannerColorContext";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
 import React from "react";
 import { colors } from "@upswyng/common";
-
 interface Props {
   children: React.ReactNode;
   color?: string;
@@ -19,14 +20,17 @@ const PageBanner = ({ children, color, backButtonAction }: Props) => {
   React.useEffect(() => {
     updateCurrentBannerColor(color);
   }, [color, updateCurrentBannerColor]);
-
   return (
     <Box bgcolor={currentBannerColor || colors.black} mb={4} py={2}>
       <Grid container wrap="nowrap" alignItems="center" spacing={1}>
         <Grid item>
-          <BackButton backButtonAction={backButtonAction} />
+          <IconButton>
+            <BackButton backButtonAction={backButtonAction} />
+          </IconButton>
         </Grid>
-        <Grid item>{children}</Grid>
+        <List>
+          <Grid item>{children}</Grid>
+        </List>
       </Grid>
     </Box>
   );
